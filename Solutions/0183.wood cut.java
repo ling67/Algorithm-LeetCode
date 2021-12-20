82bd5ee466dd108576111e40508149606953d146
ling67 <93315926+ling67@users.noreply.github.com>
2021-12-21 06:24:37 +0800

    Create 0183.wood cut.java

---
+++ Solutions/0183.wood cut.java
@@ -0,0 +1,65 @@
+/*
+描述
+Given n pieces of wood with length L[i] (integer array). Cut them into small pieces to guarantee you could have equal or more than k pieces with the same length. What is the longest length you can get from the n pieces of wood? Given L & k, return the maximum length of the small pieces.
+
+The unit of length is centimeter.The length of the woods are all positive integers,you couldn't cut wood into float length.If you couldn't get >= k pieces, return 0.
+
+样例
+Example 1
+
+Input:
+L = [232, 124, 456]
+k = 7
+Output: 114
+Explanation: We can cut it into 7 pieces if any piece is 114cm long, however we can't cut it into 7 pieces if any piece is 115cm long.
+Example 2
+
+Input:
+L = [1, 2, 3]
+k = 7
+Output: 0
+Explanation: It is obvious we can't make it.
+挑战
+O(n log Len), where Len is the longest length of the wood.
+*/
+
+public class Solution {
+    /**
+     * @param L: Given n pieces of wood with length L[i]
+     * @param k: An integer
+     * @return: The maximum length of the small pieces
+     */
+    public int woodCut(int[] L, int k) {
+        // write your code here
+        int max = 0;
+        for (int i = 0; i < L.length; i++) {
+            max = Math.max(max, L[i]);
+        }
+
+        int start = 1, end = max;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (count(L, mid) >= k) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (count(L, end) >= k) {
+            return end;
+        }
+        if (count(L, start) >= k) {
+            return start;
+        }
+        return 0;
+    }
+
+    private int count (int[] L, int lendth) {
+        int sum = 0;
+        for (int i = 0; i < L.length; i++){
+            sum += L[i] / length;
+        }
+    }
+
+}
