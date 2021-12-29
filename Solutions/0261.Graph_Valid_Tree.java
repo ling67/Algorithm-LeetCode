cef78bfb8fa9b4cf1bfd2531ccda98cea0db064c
ling67 <93315926+ling67@users.noreply.github.com>
2021-12-29 12:58:56 +0800

    Create 0261.Graph_Valid_Tree.java

---
+++ Solutions/0261.Graph_Valid_Tree.java
@@ -0,0 +1,83 @@
+/*
+You have a graph of n nodes labeled from 0 to n - 1. You are given an integer n and a list of edges where edges[i] = [ai, bi] indicates that there is an undirected edge between nodes ai and bi in the graph.
+
+Return true if the edges of the given graph make up a valid tree, and false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: n = 5, edges = [[0,1],[0,2],[0,3],[1,4]]
+Output: true
+Example 2:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]]
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 2000
+0 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai, bi < n
+ai != bi
+There are no self-loops or repeated edges.
+*/
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        
+        if (n == 0) {
+            return false;
+        }
+        
+        //1.边的个数为点的个数-1
+        if (edges.length != n-1) {
+            return false;
+        }
+        
+        Map<Integer, Set<Integer>> graph = initializeGraph(n, edges);
+        
+        //2.判断连通性，就是通过一个点把其他的点都能找到
+        //bfs
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> hash = new HashSet<>();
+        
+        queue.offer(0);
+        hash.add(0);
+        
+        int visit = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            visit++;
+            for (Integer neighbor : graph.get(node)) {
+                if (hash.contains(neighbor)) {
+                    continue;
+                }
+                hash.add(neighbor);
+                queue.offer(neighbor);
+            }
+        }
+        return visit == n;
+    }
+    
+    private Map<Integer, Set<Integer>> initializeGraph(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new HashSet<Integer>());
+        }
+        
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        
+        return graph;
+        
+    }
+}
