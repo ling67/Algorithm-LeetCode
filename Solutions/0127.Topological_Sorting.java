8618d64b88e853aed1c2f0a2dc5217f6834ba2e3
ling67 <93315926+ling67@users.noreply.github.com>
2021-12-29 12:35:18 +0800

    Create 0127.Topological_Sorting.java

---
+++ Solutions/0127.Topological_Sorting.java
@@ -0,0 +1,104 @@
+/*
+描述
+给定一个有向图，图节点的拓扑排序定义如下:
+
+对于图中的每一条有向边 A -> B , 在拓扑排序中A一定在B之前.
+拓扑排序中的第一个节点可以是图中的任何一个没有其他节点指向它的节点.
+针对给定的有向图找到任意一种拓扑排序的顺序.
+
+你可以假设图中至少存在一种拓扑排序
+有关图的表示详情请看这里
+
+图结点的个数 <= 5000
+样例
+样例 1：
+
+输入：
+
+graph = {0,1,2,3#1,4#2,4,5#3,4,5#4#5}
+输出：
+
+[0, 1, 2, 3, 4, 5]
+解释：
+
+图如下所示:
+
+picture
+
+拓扑排序可以为:
+[0, 1, 2, 3, 4, 5]
+[0, 2, 3, 1, 5, 4]
+...
+您只需要返回给定图的任何一种拓扑顺序。
+
+挑战
+能否分别用BFS和DFS完成？
+*/
+
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     List<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) {
+ *         label = x;
+ *         neighbors = new ArrayList<DirectedGraphNode>();
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+
+        ArrayList<DirectedGraphNode> order = new ArrayList<>();
+
+        if (graph == null) {
+            return order;
+        }
+
+        //1.count indegree
+        Map<DirectedGraphNode, Integer> indegree = getIndegree(graph); 
+
+        //2.topological sorting - put all nodes that indgree = 0 into queue bfs 为什么不需要hashset，因为靠的是indegree中是否为0控制的是否进入队列，不可能两次为0
+        Queue<DirectedGraphNode> queue = new LinkedList<>();
+        for (DirectedGraphNode node : graph) {
+            if (indegree.get(node) == 0) {
+                queue.offer(node);
+                order.add(node);
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            DirectedGraphNode node = queue.poll();
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                indegree.put(neighbor, indegree.get(neighbor) - 1);
+                if (indegree.get(neighbor) == 0) {
+                    queue.offer(neighbor);
+                    order.add(neighbor);
+                }
+            }
+        }
+
+        return order;
+    }
+
+    private Map<DirectedGraphNode, Integer> getIndegree(ArrayList<DirectedGraphNode> graph) {
+        Map<DirectedGraphNode, Integer> indegree = new HashMap<>();
+        for (DirectedGraphNode node : graph) {
+            indegree.put(node, 0);
+        } 
+        for (DirectedGraphNode node : graph) {
+            for (DirectedGraphNode neighbor : node.neighbors) {
+                indegree.put(neighbor, indegree.get(neighbor) + 1);
+            }
+        }
+        return indegree;
+    }
+}
+
+
