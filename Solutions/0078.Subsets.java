2a215c70e1d575ca54fe72245486976c9f9af4dd
ling67 <93315926+ling67@users.noreply.github.com>
2021-12-31 03:06:08 +0800

    Update 0078.Subsets.java

--- Solutions/0078.Subsets.java
+++ Solutions/0078.Subsets.java
@@ -1,3 +1,79 @@
+/*
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+*/
+
+//通用的深度优先搜索方法
+// 递归：实现方式，一种实现DFS算法的一种方式
+class Solution {
+    /**
+     * @param S: A set of numbers.
+     * @return: A list of lists. All valid subsets.
+     */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        
+        if (nums == null) {
+            return results;
+        }
+        
+        if (nums.length == 0) {
+            results.add(new ArrayList<Integer>());
+            return results;
+        }
+        
+        Arrays.sort(nums);
+        helper(new ArrayList<Integer>(), nums, 0, results);
+        return results;
+    }
+    
+    
+    // 递归三要素
+    // 1. 递归的定义：在 Nums 中找到所有以 subset 开头的的集合，并放到 results
+    private void helper(ArrayList<Integer> subset,
+                        int[] nums,
+                        int startIndex,
+                        List<List<Integer>> results) {
+        // 2. 递归的拆解
+        // deep copy
+        // results.add(subset);
+        results.add(new ArrayList<Integer>(subset));
+        
+        for (int i = startIndex; i < nums.length; i++) {
+            // [1] -> [1,2]
+            subset.add(nums[i]);
+            // 寻找所有以 [1,2] 开头的集合，并扔到 results
+            helper(subset, nums, i + 1, results);
+            // [1,2] -> [1]  回溯
+            subset.remove(subset.size() - 1);
+        }
+        
+        // 3. 递归的出口
+        // return;
+    }
+}
+
+
+
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
