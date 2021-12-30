66870321755317678e1d86b95efd01b4333328be
ling67 <93315926+ling67@users.noreply.github.com>
2021-12-31 02:38:00 +0800

    Update 0078.Subsets.java

--- Solutions/0078.Subsets.java
+++ Solutions/0078.Subsets.java
@@ -1,28 +1,3 @@
-/*
-Given an integer array nums of unique elements, return all possible subsets (the power set).
-
-The solution set must not contain duplicate subsets. Return the solution in any order.
-
- 
-
-Example 1:
-
-Input: nums = [1,2,3]
-Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
-Example 2:
-
-Input: nums = [0]
-Output: [[],[0]]
- 
-
-Constraints:
-
-1 <= nums.length <= 10
--10 <= nums[i] <= 10
-All the numbers of nums are unique.
-
-*/
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
@@ -31,12 +6,12 @@ class Solution {
         return results;
     }
     
-    //1.definition
+    //1.definition，Java对象传递时传的是引用，而不是值，所以在子函数中改变对象的值，调用函数也会改变
     private void dfs(int[] nums, int index, ArrayList<Integer> subset, List<List<Integer>> results) {
         
         //3.exit
         if (index == nums.length) {
-            results.add(new ArrayList<Integer>(subset));
+            results.add(new ArrayList<Integer>(subset));  //这里必须新建ArrayList，而不是直接赋值，因为subset会变
             return;
         }
         
@@ -52,4 +27,3 @@ class Solution {
         
     }
 }
-
