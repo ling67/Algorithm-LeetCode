0b133c1a2f33daea85068508a22c9aa5ce5afe0c
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-02 07:27:25 +0800

    Create 0141.Linked_List_Cycle.java

---
+++ Solutions/0141.Linked_List_Cycle.java
@@ -0,0 +1,72 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+*/
+
+
+//version1:hash
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> hash = new HashSet<ListNode>();
+        ListNode curt = head;
+        while (curt != null) {
+            if (hash.contains(curt)) {
+                return true;
+            }
+            hash.add(curt);
+            curt = curt.next;
+        }
+        return false;
+    }
+}
+
+//version2:o(1)
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slowNode = head;
+        ListNode quickNode = head.next;
+        
+        while (slowNode != quickNode) {
+            if (slowNode == null || quickNode == null || quickNode.next == null) {
+                return false;
+            }
+            slowNode = slowNode.next;
+            quickNode = quickNode.next.next;
+        }
+        return true;
+    }
+}
+
+
+
