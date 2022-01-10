f35ccee6ff76d9df7871d36d8981a1f50f1f0291
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-11 04:07:08 +0800

    Create 0045.Jump_Game_II.java

---
+++ Solutions/0045.Jump_Game_II.java
@@ -0,0 +1,44 @@
+/*
+Given an array of non-negative integers nums, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Your goal is to reach the last index in the minimum number of jumps.
+
+You can assume that you can always reach the last index.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+*/
+
+/*
+1.definition dp[i] represent the minimum number of jumps to reach the i index 
+2.we require dp[n-1]
+3.initialization initialize the dp[0] = 0
+4.recursion fomular dp[i] = min(d[j] + 1) each j from 0 to i-1 && A[j] >= i-j
+*/
+class Solution {
+    public int jump(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] >= i - j) {
+                    dp[i] = Math.min(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp[n-1];
+    }
+}
