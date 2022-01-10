e2041f5d8b5a4d705cded3b9a3ae19574def4f04
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-11 06:24:56 +0800

    Create 0716.Max Stack.java

---
+++ Solutions/0716.Max Stack.java
@@ -0,0 +1,88 @@
+/*
+Design a max stack data structure that supports the stack operations and supports finding the stack's maximum element.
+
+Implement the MaxStack class:
+
+MaxStack() Initializes the stack object.
+void push(int x) Pushes element x onto the stack.
+int pop() Removes the element on top of the stack and returns it.
+int top() Gets the element on the top of the stack without removing it.
+int peekMax() Retrieves the maximum element in the stack without removing it.
+int popMax() Retrieves the maximum element in the stack and removes it. If there is more than one maximum element, only remove the top-most one.
+ 
+
+Example 1:
+
+Input
+["MaxStack", "push", "push", "push", "top", "popMax", "top", "peekMax", "pop", "top"]
+[[], [5], [1], [5], [], [], [], [], [], []]
+Output
+[null, null, null, null, 5, 5, 1, 5, 1, 5]
+
+Explanation
+MaxStack stk = new MaxStack();
+stk.push(5);   // [5] the top of the stack and the maximum number is 5.
+stk.push(1);   // [5, 1] the top of the stack is 1, but the maximum is 5.
+stk.push(5);   // [5, 1, 5] the top of the stack is 5, which is also the maximum, because it is the top most one.
+stk.top();     // return 5, [5, 1, 5] the stack did not change.
+stk.popMax();  // return 5, [5, 1] the stack is changed now, and the top is different from the max.
+stk.top();     // return 1, [5, 1] the stack did not change.
+stk.peekMax(); // return 5, [5, 1] the stack did not change.
+stk.pop();     // return 1, [5] the top of the stack and the max element is now 5.
+stk.top();     // return 5, [5] the stack did not change.
+ 
+*/
+
+class MaxStack {
+    
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
+
+    public MaxStack() {
+        stack = new Stack();
+        maxStack = new Stack();
+    }
+    
+    public void push(int x) {
+        if(maxStack.isEmpty() || x >= maxStack.peek()) {
+            maxStack.push(x);
+        }
+        stack.push(x);
+    }
+    
+    public int pop() {
+        if (maxStack.peek() == stack.peek()) {
+            maxStack.pop();
+        }
+        return stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int peekMax() {
+        return maxStack.peek();
+    }
+    
+    public int popMax() {
+        int max = peekMax();
+        Stack<Integer> buffer = new Stack();
+        while (top() != max) buffer.push(pop());
+        pop();
+        while (!buffer.isEmpty()) push(buffer.pop());
+        return max;
+    }
+}
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack obj = new MaxStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.peekMax();
+ * int param_5 = obj.popMax();
+ */
+
+
