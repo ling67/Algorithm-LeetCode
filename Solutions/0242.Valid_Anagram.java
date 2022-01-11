7343960a7555db689931cd64ab8a14ca33a561a0
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-12 03:30:44 +0800

    Create 0242.Valid_Anagram.java

---
+++ Solutions/0242.Valid_Anagram.java
@@ -0,0 +1,39 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 5 * 104
+s and t consist of lowercase English letters.
+*/
+
+/*
+approach 1:sort
+approach 2:int count[]
+approach 3:hashtable
+
+*/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
