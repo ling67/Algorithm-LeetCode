060b19ac0245fec390094c3f02090531f91a5c05
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-15 00:34:42 +0800

    Create 0240.Search_a_2D_Matrix_II.java

---
+++ Solutions/0240.Search_a_2D_Matrix_II.java
@@ -0,0 +1,45 @@
+/*
+Write an efficient algorithm that searches for a target value in an m x n integer matrix. The matrix has the following properties:
+
+Integers in each row are sorted in ascending from left to right.
+Integers in each column are sorted in ascending from top to bottom.
+ 
+
+Example 1:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+Output: true
+Example 2:
+
+
+Input: matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+Output: false
+*/
+
+/*
+method 1:brute force
+method 2:binary search  
+*/
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = m - 1, j = 0;
+        
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
