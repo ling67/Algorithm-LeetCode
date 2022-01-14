6115a6a66b06a105284f671b2a8f8041fd937e8e
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-14 11:46:28 +0800

    Update 0278.First_Bad_Version.java

--- Solutions/0278.First_Bad_Version.java
+++ Solutions/0278.First_Bad_Version.java
@@ -32,25 +32,15 @@ Constraints:
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        
         int start = 1, end = n;
-        
-        while(start + 1 < end) {
-            int mid = start + (end - start)/2;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
             if (isBadVersion(mid)) {
                 end = mid;
             } else {
                 start = mid;
             }
         }
-        
-        if (isBadVersion(start)) {
-            return start;
-        }
-        if (isBadVersion(end)) {
-            return end;
-        }
-        
-        return 0;
+        return isBadVersion(start) ? start : end;
     }
 }
