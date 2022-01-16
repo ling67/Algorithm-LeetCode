a62b0b066f79e90d098cca4e591ad8310375f4cc
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-16 11:59:46 +0800

    Update 0094.Binary_Tree_Inorder_Traversal.java

--- Solutions/0094.Binary_Tree_Inorder_Traversal.java
+++ Solutions/0094.Binary_Tree_Inorder_Traversal.java
@@ -43,25 +43,17 @@ The number of nodes in the tree is in the range [0, 100].
 
 //version 1:divide & conque
 class Solution {
-    public ArrayList<Integer> inorderTraversal(TreeNode root) {
-        
-        ArrayList<Integer> result = new ArrayList<Integer>();
-        
-        if(root == null){
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        if (root == null) {
             return result;
         }
-        
-        ArrayList<Integer> left = inorderTraversal(root.left);
-        ArrayList<Integer> right = inorderTraversal(root.right);
-        
-        result.addAll(left);
+        result.addAll(inorderTraversal(root.left));
         result.add(root.val);
-        result.addAll(right);
-        
+        result.addAll(inorderTraversal(root.right));
         return result;
     }
 }
-
 
 //version 2:Traverse
 
