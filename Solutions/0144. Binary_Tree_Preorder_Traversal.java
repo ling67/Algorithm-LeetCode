14ecf7e0f772428ea4925418e6c147561ec5bec5
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-17 09:07:08 +0800

    Update 0144. Binary_Tree_Preorder_Traversal.java

--- Solutions/0144. Binary_Tree_Preorder_Traversal.java
+++ Solutions/0144. Binary_Tree_Preorder_Traversal.java
@@ -64,5 +64,26 @@ class Solution {
 
 
 //version 3:Non-Recursion 
-
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        List<Integer> preorder = new ArrayList<>();
+        
+        if (root == null) {
+            return preorder;
+        }
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode node = stack.pop();
+            preorder.add(node.val);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return preorder;
+    }
+}
 
