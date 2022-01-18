fcd882b3a0d231c45fd2a43df802d773cc2f25d7
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-18 12:29:53 +0800

    Create 0230.Kth_Smallest_Element_in_a_BST.java

---
+++ Solutions/0230.Kth_Smallest_Element_in_a_BST.java
@@ -0,0 +1,49 @@
+/*
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+
+
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        ArrayList<Integer> result = inorder(root, new ArrayList<Integer>());
+        return result.get(k - 1);
+    }
+    
+    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr){
+        if (root == null) return arr;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+        return arr;
+    }
+}
