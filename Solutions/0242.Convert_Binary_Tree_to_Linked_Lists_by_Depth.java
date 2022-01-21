bd9db59a60d37d60bfdee72c5b2f476f0f12b61f
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-21 12:19:43 +0800

    Create 0242.Convert_Binary_Tree_to_Linked_Lists_by_Depth.java

---
+++ Solutions/0242.Convert_Binary_Tree_to_Linked_Lists_by_Depth.java
@@ -0,0 +1,83 @@
+/*
+Description
+Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+
+Wechat reply the 【242】 get the latest frequent Interview questions . (wechat id : jiuzhang15)
+
+Example
+Example 1:
+
+Input: {1,2,3,4}
+Output: [1->null,2->3->null,4->null]
+Explanation: 
+        1
+       / \
+      2   3
+     /
+    4
+Example 2:
+
+Input: {1,#,2,3}
+Output: [1->null,2->null,3->null]
+Explanation: 
+    1
+     \
+      2
+     /
+    3
+*/
+
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    /**
+     * @param root the root of binary tree
+     * @return a lists of linked list
+     */
+    public List<ListNode> binaryTreeToLists(TreeNode root) {
+        // Write your code here
+        List<ListNode> result = new LinkedList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        ListNode dummy = new ListNode(0);   //dummy node recode the node before the head
+        ListNode lastNode = null;
+        while (!queue.isEmpty()) {
+            dummy.next = null;
+            lastNode = dummy;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                lastNode.next = new ListNode(head.val);
+                lastNode = lastNode.next;
+
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+            }
+            result.add(dummy.next);
+        }
+        return result;
+    }
+}
