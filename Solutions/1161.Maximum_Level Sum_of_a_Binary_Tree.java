7a6d55c6f4cd0927a7b5db77bfa161a2c3e28c62
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-23 09:05:44 +0800

    Create 1161.Maximum_Level Sum_of_a_Binary_Tree.java

---
+++ Solutions/1161.Maximum_Level Sum_of_a_Binary_Tree.java
@@ -0,0 +1,72 @@
+/*
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+Example 2:
+
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+*/
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        int maxSum = Integer.MIN_VALUE;
+        int maxLayer = 1;
+        int layer = 1;
+        
+        while (!queue.isEmpty()) {
+            int sum = 0;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+                sum += head.val;
+            }
+            if (sum > maxSum) {
+                maxSum = sum;
+                maxLayer = layer;
+            }
+            maxSum = Math.max(maxSum, sum);
+            layer++;
+        }    
+        return maxLayer;
+    }
+}
