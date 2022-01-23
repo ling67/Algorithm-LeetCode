39b53b9b930211c66ae506295e2210b3ff0366f4
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-23 08:55:11 +0800

    Create 0515.Find_Largest_Value_in_Each_Tree_Row.java

---
+++ Solutions/0515.Find_Largest_Value_in_Each_Tree_Row.java
@@ -0,0 +1,54 @@
+/*
+Given the root of a binary tree, return an array of the largest value in each row of the tree (0-indexed).
+
+Example 1:
+Input: root = [1,3,2,5,3,null,9]
+Output: [1,3,9]
+Example 2:
+
+Input: root = [1,2,3]
+Output: [1,3]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int rowMax = Integer.MIN_VALUE;
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode head = queue.poll();
+                if (head.left != null) {
+                    queue.offer(head.left);
+                }
+                if (head.right != null) {
+                    queue.offer(head.right);
+                }
+                rowMax = Math.max(rowMax, head.val);
+            }
+            result.add(rowMax);
+            rowMax = Integer.MIN_VALUE;
+        }
+        return result;
+    }
+}
