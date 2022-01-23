958c01dc4b5c4e5d7f97ca8d2557e3d8c5045167
ling67 <93315926+ling67@users.noreply.github.com>
2022-01-23 09:14:48 +0800

    Create 0050.Pow(x, n).java

---
+++ Solutions/0050.Pow(x, n).java
@@ -0,0 +1,42 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+*/
+
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        
+        double res = 0.0;
+        double half = myPow(x, n / 2);
+        
+        if (n % 2 == 0) {
+            res = half * half;
+        } else {
+            if (n < 0) {
+                res = 1 / x * half * half;
+            } else {
+                res = x * half * half;
+            }
+        }
+        
+        return res;
+    }
+}
