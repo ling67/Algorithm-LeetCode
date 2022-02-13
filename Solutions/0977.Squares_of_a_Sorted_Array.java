6a4d713e2ed18cccb7d8b06eadf0ef4e559dd868
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-13 11:42:35 +0800

    Create 0977.Squares_of_a_Sorted_Array.java

---
+++ Solutions/0977.Squares_of_a_Sorted_Array.java
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]    
+"""
+
+  
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        i, j, k = 0, len(nums) - 1, len(nums) - 1
+        while i <= j:
+            if nums[i]**2 > nums[j]**2:
+                res[k] = nums[i]**2
+                i += 1
+            else:
+                res[k] = nums[j]**2
+                j -= 1
+            k -= 1
+        return res 
