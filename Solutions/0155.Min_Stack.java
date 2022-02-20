a8af2225c1e1de53360ea205011bb4ec268ccdcd
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-21 03:44:10 +0800

    Update 0155.Min_Stack.java

--- Solutions/0155.Min_Stack.java
+++ Solutions/0155.Min_Stack.java
@@ -80,5 +80,47 @@ class MinStack {
  */
 
 
+class MinStack:
+
+    def __init__(self):
+        self.st = []
+        self.minSt = []
+
+    def push(self, val: int) -> None:
+        self.st.append(val)
+        
+        if len(self.minSt) == 0 or val <= self.minSt[-1]:
+            self.minSt.append(val)
+
+    def pop(self) -> None:
+        if len(self.st) == 0:
+            raise IndexError("the stack is empty")
+        else:
+            poppedItem = self.st.pop()
+            if poppedItem == self.minSt[-1]:
+                self.minSt.pop()
+            return poppedItem
+
+    def top(self) -> int:
+        if len(self.st) == 0:
+            raise IndexError("the stack is empty")
+        else:
+            return self.st[-1]
+
+    def getMin(self) -> int:
+        if len(self.st) == 0:
+            raise IndexEroor("the stack is empty")
+        else:
+            return self.minSt[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+ 
+
 
 
