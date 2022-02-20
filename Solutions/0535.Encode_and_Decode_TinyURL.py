74456f71a69a8ef9856700d4aefcc1bbb2062e43
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-20 10:56:48 +0800

    Create 0535.Encode_and_Decode_TinyURL.py

---
+++ Solutions/0535.Encode_and_Decode_TinyURL.py
@@ -0,0 +1,50 @@
+"""
+Note: This is a companion problem to the System Design problem: Design TinyURL.
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk. Design a class to encode a URL and decode a tiny URL.
+
+There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+
+Implement the Solution class:
+
+Solution() Initializes the object of the system.
+String encode(String longUrl) Returns a tiny URL for the given longUrl.
+String decode(String shortUrl) Returns the original long URL for the given shortUrl. It is guaranteed that the given shortUrl was encoded by the same object.
+ 
+
+Example 1:
+
+Input: url = "https://leetcode.com/problems/design-tinyurl"
+Output: "https://leetcode.com/problems/design-tinyurl"
+
+Explanation:
+Solution obj = new Solution();
+string tiny = obj.encode(url); // returns the encoded tiny url.
+string ans = obj.decode(tiny); // returns the original url after deconding it.
+ 
+
+Constraints:
+
+1 <= url.length <= 104
+url is guranteed to be a valid URL.
+"""
+
+class Codec:
+
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        self.mapping = defaultdict(str)   #声明value的类型是str
+        hash_code = hash(longUrl)
+        self.mapping[hash_code] = longUrl
+        stortUrl = "http://tinyurl.com/" + str(hash_code)
+        return stortUrl 
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        hash_code = int(shortUrl.split("/")[-1])   #-1就是取数组最后一个数
+        return self.mapping[hash_code]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
