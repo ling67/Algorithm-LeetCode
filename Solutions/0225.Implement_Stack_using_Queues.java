e1c47f3946955c726637d5c053c076c85aafe15e
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-21 03:34:32 +0800

    Update 0225.Implement_Stack_using_Queues.java

--- Solutions/0225.Implement_Stack_using_Queues.java
+++ Solutions/0225.Implement_Stack_using_Queues.java
@@ -89,6 +89,37 @@ class MyStack {
  */
 
 
+class MyStack:
 
+    def __init__(self):
+        self.q1 = collections.deque()    #store element
+        self.q2 = collections.deque()    #help space
+
+    def push(self, x: int) -> None:
+        while self.q1:
+            self.q2.append(self.q1.popleft())
+        self.q1.append(x)
+        while self.q2:
+            self.q1.append(self.q2.popleft())
+
+    def pop(self) -> int:
+        if len(self.q1) == 0:
+            raise IndexError("The stack is empty")
+        return self.q1.popleft()
+
+    def top(self) -> int:
+        if len(self.q1) == 0:
+            raise IndexError("The stack is empty")
+        return self.q1[0]
+        
+    def empty(self) -> bool:
+        return len(self.q1) == 0
+        
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
 
 
