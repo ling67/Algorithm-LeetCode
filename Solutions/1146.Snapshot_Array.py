e81945494e92fbf00b9da1438f0748101ce5869e
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-20 11:20:39 +0800

    Create 1146.Snapshot_Array.py

---
+++ Solutions/1146.Snapshot_Array.py
@@ -0,0 +1,52 @@
+"""
+Implement a SnapshotArray that supports the following interface:
+
+SnapshotArray(int length) initializes an array-like data structure with the given length.  Initially, each element equals 0.
+void set(index, val) sets the element at the given index to be equal to val.
+int snap() takes a snapshot of the array and returns the snap_id: the total number of times we called snap() minus 1.
+int get(index, snap_id) returns the value at the given index, at the time we took the snapshot with the given snap_id
+ 
+
+Example 1:
+
+Input: ["SnapshotArray","set","snap","set","get"]
+[[3],[0,5],[],[0,6],[0,0]]
+Output: [null,null,0,null,5]
+Explanation: 
+SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
+snapshotArr.set(0,5);  // Set array[0] = 5
+snapshotArr.snap();  // Take a snapshot, return snap_id = 0
+snapshotArr.set(0,6);
+snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
+ 
+
+Constraints:
+
+1 <= length <= 50000
+At most 50000 calls will be made to set, snap, and get.
+0 <= index < length
+0 <= snap_id < (the total number of times we call snap())
+0 <= val <= 10^9
+"""
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.idx_to_val = defaultdict(int)
+        self.snapshots = []         #stores the ind_to_val dictionary at each time
+        
+    def set(self, index: int, val: int) -> None:
+        self.idx_to_val[index] = val
+
+    def snap(self) -> int:
+        self.snapshots.append(self.idx_to_val.copy())     #now it is O(N), which is << O(L) if sparse
+        return len(self.snapshots) - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        return self.snapshots[snap_id][index]
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
