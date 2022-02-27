5638aec9ac600c8818397e0e5d440cc2ef16a9b1
ling67 <93315926+ling67@users.noreply.github.com>
2022-02-27 12:12:57 +0800

    Create 0658.Find_K_Closest_Elements.py

---
+++ Solutions/0658.Find_K_Closest_Elements.py
@@ -0,0 +1,38 @@
+"""
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+ 
+
+Example 1:
+
+Input: arr = [1,2,3,4,5], k = 4, x = 3
+Output: [1,2,3,4]
+Example 2:
+
+Input: arr = [1,2,3,4,5], k = 4, x = -1
+Output: [1,2,3,4]
+ 
+
+Constraints:
+
+1 <= k <= arr.length
+1 <= arr.length <= 104
+arr is sorted in ascending order.
+-104 <= arr[i], x <= 104
+"""
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        hq = []
+        res = []
+        for num in arr:
+            hq.append((abs(num - x), num))
+        heapify(hq)
+        for _ in range(k):
+            res.append(heappop(hq)[1])
+        res.sort()
+        return res
