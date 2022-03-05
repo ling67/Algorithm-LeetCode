726e7c7b33dc1d1f6f364b35e1dfe2192403ff51
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-06 03:40:47 +0800

    Update 0055.Jump_Game.java

--- Solutions/0055.Jump_Game.java
+++ Solutions/0055.Jump_Game.java
@@ -18,6 +18,24 @@ Explanation: You will always arrive at index 3 no matter what. Its maximum jump 
 
 */
 
+"""
+1.确定状态dp[i] 能不能跳到i
+2.求dp[n-1]
+3.初始化：dp[0] = true
+4.递推公式 dp[i] = (0<j<i)dp[j] + a[j] > i
+"""
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        dp = [False] * len(nums)
+        dp[0] = True
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if dp[j] + nums[j] >= i:
+                    dp[i] = True
+                    break
+        return dp[-1]
+
+
 /*
 1.definition dp[i] represents the result of whether it is possible to skip from index 0 to the index i position
 2.we require dp[n-1]
