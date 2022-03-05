defdd8cf1c88f65db656c88c9efb9a6f0a23049c
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-06 05:31:17 +0800

    Update 0063.Unique_Paths_II.java

--- Solutions/0063.Unique_Paths_II.java
+++ Solutions/0063.Unique_Paths_II.java
@@ -26,6 +26,48 @@ Output: 1
 
 */
 
+"""
+1.确定状态：dp[i][j]走到i，j有多少种方案
+2.求dp[m-1][n-1]
+3.初始化 dp[0][0] = 1
+4.dp[i][j] = 1, 如果有障碍， dp[i-1][j]+dp[i][j-1]
+"""
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        if obstacleGrid[0][0] == 1:
+            return 0;
+        
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]   # 确定状态：f[i][j]表示达到(i, j)位置的方法数
+        
+        #初始化
+        dp[0][0] = 1
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 0:
+                dp[i][0] = dp[i-1][0]
+            else:
+                dp[i][0] = 0
+        
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 0:
+                dp[0][j] = dp[0][j-1]
+            else:
+                dp[0][j] = 0
+            
+        #递推公式    
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                    
+        return dp[m-1][n-1]
+                
+        
+        
+
+
 /*
 1.状态定义dp[i][j]代表从（0，0）出发到达（i，j）的路径条数
 2.求dp[m-1][n-1]
