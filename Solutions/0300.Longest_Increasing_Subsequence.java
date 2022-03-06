2ca9bad7ab56c0322ca627e7ecaaeb9605cb831d
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-07 07:48:31 +0800

    Update 0300.Longest_Increasing_Subsequence.java

--- Solutions/0300.Longest_Increasing_Subsequence.java
+++ Solutions/0300.Longest_Increasing_Subsequence.java
@@ -46,7 +46,27 @@ then print
 
 """NlogN的算法，还是网上的高人讲得好。https://www.youtube.com/watch?v=YoeWZ3ELMEk"""
 //version2: python
-
+"""
+dp是后退，nlogn选择正推
+1.状态定义：dp[i]以i结尾的最长子序列
+2.求：dp[i]最大值
+3.初始化：dp[0] = 1
+4.递推公式：dp[i] = max(dp[j]+1) for j < i && nums[j] < nums[i];
+"""
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [0] * len(nums)
+        size = 0
+        
+        for num in nums:
+            #[40, 60] 10 [10, 60] 20 left side < 20
+        
+            i = bisect.bisect_left(dp, num, 0, size)  #0, 2 val < dp[i]   #i返回的是左边都比index小
+            dp[i] = num
+            if i == size: # 4,5,6
+                size += 1
+            
+        return size
              
              
 //version3: java
