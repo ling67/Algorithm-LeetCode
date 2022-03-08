0ff6105f247893737b1bab52313bf306e0ffe579
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-08 14:00:46 +0800

    Update 0034.Find_First_and_Last_Position_of_Element_in_Sorted_Array.java

--- Solutions/0034.Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
+++ Solutions/0034.Find_First_and_Last_Position_of_Element_in_Sorted_Array.java
@@ -5,6 +5,52 @@ why？ 因为取中点除2的运算是偏左的
 所以first+last整合成一个模板去写，所以start+1<end任何情况都成立，但是要做double check
 **/
 
+"""
+find the first value 
+find the last value
+2 time binary search
+"""
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        
+        #find the first index value
+        start, end = 0, len(nums) - 1
+        first, last = -1, -1
+        while start + 1 < end:
+            mid = start +  (end - start) // 2
+            if nums[mid] >= target:
+                end = mid
+            else:
+                start = mid
+        
+        
+        if nums[end] == target:
+            first = end
+            
+        if nums[start] == target:  ## 注意由于是想找first position of target，所以把start放在后面更新，这样如果出现nums[end]和nums[start]都等于target的情况的话，first可以被后面较小的start替换掉。
+            first = start    
+        
+        #find the last index value
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = start +  (end - start) // 2
+            if nums[mid] <= target:
+                start = mid
+            else:
+                end = mid
+        
+        
+        if nums[start] == target:
+            last = start
+        if nums[end] == target:
+            last = end
+            
+        return [first, last]
+
+
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
