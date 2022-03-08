bae62903cf843c367dd5a20faf0cafa5a5303ed7
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-08 14:13:05 +0800

    Update 1287.Element_Appearing_More_Than_25%_In_Sorted_Array.java

--- Solutions/1287.Element_Appearing_More_Than_25%_In_Sorted_Array.java
+++ Solutions/1287.Element_Appearing_More_Than_25%_In_Sorted_Array.java
@@ -20,6 +20,48 @@ Constraints:
 
 */
 
+"""
+element more than 25% means 1/4n, 1/2n, 3/4n
+"""
+class Solution:
+    def findSpecialInteger(self, arr: List[int]) -> int:
+        n = len(arr)
+        num1 = arr[n // 4]
+        num2 = arr[n // 2]
+        num3 = arr[n * 3 // 4] 
+        
+        first1 = self.binary_search_first(arr, num1)
+        last1 = self.binary_search_last(arr, num1)
+        if last1 - first1 >= n // 4:
+            return num1
+        
+        first2 = self.binary_search_first(arr, num2)
+        last2 = self.binary_search_last(arr, num2)
+        if last2 - first2 >= n // 4:
+            return num2
+        return num3
+        
+    def binary_search_first(self, arr, num):
+        start, end = 0, len(arr) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if arr[mid] >= num:
+                end = mid
+            else:
+                start = mid
+        return start if arr[start] == num else end
+        
+    def binary_search_last(self, arr, num):
+        start, end = 0, len(arr) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if arr[mid] <= num:
+                start = mid
+            else:
+                end = mid
+        return end if arr[end] == num else start
+        
+
 
 class Solution {
     public int findSpecialInteger(int[] arr) {
