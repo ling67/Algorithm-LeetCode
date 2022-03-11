0bdd0d3a6839500c5c75c18e83f91f30f8a0c037
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-12 03:02:45 +0800

    Update 0617.Merge_Two_Binary_Trees.java

--- Solutions/0617.Merge_Two_Binary_Trees.java
+++ Solutions/0617.Merge_Two_Binary_Trees.java
@@ -20,6 +20,30 @@ Input: root1 = [1], root2 = [1,2]
 Output: [2,2]
 */
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        #exit
+        if not root1 and not root2:
+            return None
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        
+        #devide
+        left = self.mergeTrees(root1.left, root2.left)
+        right = self.mergeTrees(root1.right, root2.right)
+        
+        #conque
+        root = TreeNode(root1.val+root2.val)
+        root.left, root.right = left, right
+        return root
 
 /**
  * Definition for a binary tree node.
