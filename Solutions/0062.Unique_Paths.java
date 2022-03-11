fc46116df95710bab344313acd10afe0f57a58ec
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-11 14:02:56 +0800

    Update 0062.Unique_Paths.java

--- Solutions/0062.Unique_Paths.java
+++ Solutions/0062.Unique_Paths.java
@@ -17,7 +17,7 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 1. Right -> Down -> Down
 2. Down -> Down -> Right
 3. Down -> Right -> Down
-
+time complexity: O(m*n)
 */
 
 """
