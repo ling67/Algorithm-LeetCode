2d0a7f309f4b62992f8521f89b799829aed02f3d
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-15 07:08:06 +0800

    Update 0104.Maximum_Depth_of_Binary_Tree.java

--- Solutions/0104.Maximum_Depth_of_Binary_Tree.java
+++ Solutions/0104.Maximum_Depth_of_Binary_Tree.java
@@ -41,6 +41,21 @@ The number of nodes in the tree is in the range [0, 104].
 
 
 //version 1: divide
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        
+        #devide & conquer
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+                   
+
 class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
