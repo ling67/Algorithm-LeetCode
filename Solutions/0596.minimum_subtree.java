a275ab67166d9b8465d3737f366b6ed074ecc7b3
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-15 07:31:34 +0800

    Update 0596.minimum_subtree.java

--- Solutions/0596.minimum_subtree.java
+++ Solutions/0596.minimum_subtree.java
@@ -30,6 +30,53 @@ LintCode会打印根节点为你返回节点的子树。保证只有一棵和最
 这棵树只有整体这一个子树，所以返回1.
 */
 
+from lintcode import (
+    TreeNode,
+)
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+#python 版本             
+class Solution:
+    """
+    @param root: the root of binary tree
+    @return: the root of the minimum subtree
+    """
+    def find_subtree(self, root: TreeNode) -> TreeNode:
+        # write your code here
+        if not root:
+            return None
+
+        self.minSum = float("inf")
+        self.minNode = root
+
+        self.helper(root)
+        return self.minNode
+
+    def helper(self, root) -> int:
+        if not root:
+            return 0
+        #divide
+        leftSum = self.helper(root.left)
+        rightSum = self.helper(root.right)
+        
+        #conquer
+        subSum = leftSum + rightSum + root.val
+
+        if subSum < self.minSum:
+            self.minSum = subSum
+            self.minNode = root
+
+        return subSum
+
+
+
 
 /**
  * Definition of TreeNode:
