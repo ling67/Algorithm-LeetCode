65ac98d10a3796b64f4eb0035b2718bb10d0fa4d
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-16 03:54:42 +0800

    Update and rename 0669.Trim a_Binary_Search_Tree.java to 0669.Trim a_Binary_Search_Tree.py

--- Solutions/0669.Trim a_Binary_Search_Tree.py
+++ Solutions/0669.Trim a_Binary_Search_Tree.py
@@ -17,6 +17,28 @@ Input: root = [3,0,4,null,2,null,null,1], low = 1, high = 3
 Output: [3,2,null,1]
 */
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        elif root.val < low:
+            return self.trimBST(root.right, low, high)
+
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+
+        return root
+        
+        
 
 /**
  * Definition for a binary tree node.
