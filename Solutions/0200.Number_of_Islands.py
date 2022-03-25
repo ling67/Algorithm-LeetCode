b19ecc53b47815261da8071905e7451a7eff6fe4
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-25 13:25:01 -0700

    Update 0200.Number_of_Islands.py

--- Solutions/0200.Number_of_Islands.py
+++ Solutions/0200.Number_of_Islands.py
@@ -66,9 +66,30 @@ class Solution:
                 if 0 <= adj_x < m and 0 <= adj_y < n and grid[adj_x][adj_y] == "1":
                     q.append((adj_x, adj_y))
                     grid[adj_x][adj_y] = "0"
+ 
+ //DFS python
+ class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(curr_i, curr_j):
+            visited.add((curr_i, curr_j))
+            for delta_i, delta_j in [(1,0), (-1,0), (0,1), (0,-1)]:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == "1":
+                    if (next_i, next_j) not in visited:
+                        dfs(next_i, next_j)
         
+        cnt = 0
+        m, n = len(grid), len(grid[0])
+        visited = set()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1" and (i, j) not in visited:
+                    dfs(i, j)
+                    cnt += 1
+        return cnt
                         
 
+//java version        
 //标定坐标点
 class Coordinate {
     int x, y;
