182fa6800502a6bd9535b23a44b3f9a1dbae08d6
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-25 15:36:14 -0700

    Update and rename 1020.Number_of_Enclaves.java to 1020.Number_of_Enclaves.py

--- Solutions/1020.Number_of_Enclaves.py
+++ Solutions/1020.Number_of_Enclaves.py
@@ -22,6 +22,32 @@ Explanation: All 1s are either on the boundary or can reach the boundary.
 
 */
 
+//python dfs
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def dfs(curr_i, curr_j):
+            visited.add((curr_i, curr_j))
+            self.cnt += 1
+            if curr_i == 0 or curr_i == m-1 or curr_j == 0 or curr_j == n-1:
+                self.touching_border = True  #注意：就算是true也要继续做dfs,这样可以把不符合条件的都放入visited中
+            for delta_i, delta_j in [(1,0),(-1,0),(0,1),(0,-1)]:
+                next_i, next_j = curr_i + delta_i, curr_j + delta_j
+                if 0 <= next_i < m and 0 <= next_j < n and grid[next_i][next_j] == 1:
+                    if (next_i, next_j) not in visited:
+                        dfs(next_i, next_j)
+                
+        m, n = len(grid), len(grid[0])
+        visited= set()
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (i,j) not in visited:
+                    self.touching_border = False
+                    self.cnt = 0
+                    dfs(i,j)
+                    if not self.touching_border:
+                        res += self.cnt
+        return res
 
 /*
 step1: 将四周为1的连通的陆地全部变成0
