1516d3cda1b6ec244c7c4c98f23d6cf9bb0a26b2
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-26 00:37:12 -0700

    Update 0131.Palindrome_Partitioning.java

--- Solutions/0131.Palindrome_Partitioning.java
+++ Solutions/0131.Palindrome_Partitioning.java
@@ -15,6 +15,41 @@ Input: s = "a"
 Output: [["a"]]
 */
 
+//python dfs+backtrack
+
+"""
+要求输出所有的可能组合，所以只能backtrack. O(L*2^L), where L is the lens of string, 2 is two choices: 这这里分还是不分。  
+如果题目只是要求输出所有可能组合的数目，那就dp - O(L^2)
+end condition: curr_idx == len(s) - 1
+constraint on next candidate: next_s should be palin
+parameters pass into backtrack: (curr_idx, curr_comb)
+"""
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def is_palind(t):       # O(L)
+                i, j = 0, len(t) - 1
+                while i < j:
+                    if t[i] != t[j]:
+                        return False
+                    i += 1
+                    j -= 1
+                return True        
+        
+        def backtrack(curr_idx, curr_comb):
+            if curr_idx == len(s) - 1:
+                res.append(curr_comb.copy())
+                return
+            for next_idx in range(curr_idx + 1, len(s)):
+                next_s = s[curr_idx + 1: next_idx + 1]
+                if is_palind(next_s):
+                    curr_comb.append(next_s)
+                    backtrack(next_idx, curr_comb)
+                    curr_comb.pop()
+        
+        res = []
+        backtrack(-1, [])
+        return res
+
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> results = new ArrayList<>();
