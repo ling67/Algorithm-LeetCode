f5891d9547718a13054a3d2b1f73d9938b1901a3
ling67 <93315926+ling67@users.noreply.github.com>
2022-03-25 21:07:43 -0700

    Update 0090.Subsets_II.java

--- Solutions/0090.Subsets_II.java
+++ Solutions/0090.Subsets_II.java
@@ -21,6 +21,29 @@ Constraints:
 -10 <= nums[i] <= 10
 */
 
+//version python dfs + backtrack
+
+"""
+如果输入存在重复元素，[1, 2, 2]的遍历中，我们只取前面的那个2，对于后面的那个2，如果不是挨着前面那个2选的，也就是说 i != startIndex，那么就不要放后面那个2，这样会造成重复出现[1,第一个2],[1,第二个2], 注意可以挨着第一个2来选第二个2是可以的，因为允许出现[1,2,2]作为答案。所以contraint是: if (i >= 1 and nums[i] == nums[i-1]) and i != startIndex: continue
+"""
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(curr_idx, curr_subsets):
+            res.append(curr_subsets.copy())     #has to be a deep copy
+            for next_idx in range(curr_idx + 1, len(nums)):   #不允许一个数取多次，则next candidate从i+1开始
+                if next_idx > 0 and nums[next_idx] == nums[next_idx - 1] and next_idx != curr_idx + 1:    #注意：这里的判断条件
+                    continue
+                curr_subsets.append(nums[next_idx])
+                backtrack(next_idx, curr_subsets)
+                curr_subsets.pop()
+        
+        nums.sort()      # 去重step 1: sort the nums!!!!
+        res = []
+        backtrack(-1, [])  #注意从-1开始
+        return res
+    
+
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> results = new ArrayList<>();
