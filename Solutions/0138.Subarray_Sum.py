2b1115e9770064c10065e4ae0a1816a41a1e25b5
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-03 15:29:41 -0700

    Update and rename 0138.Subarray_Sum.java to 0138.Subarray_Sum.py

--- Solutions/0138.Subarray_Sum.py
+++ Solutions/0138.Subarray_Sum.py
@@ -12,7 +12,27 @@ Output: [0, 2] or [1, 3].
 Explanation: return anyone that the sum is 0.
 */
 
+class Solution:
+    """
+    @param nums: A list of integers
+    @return: A list of integers includes the index of the first number and the index of the last number
+    """
+    def subarraySum(self, nums):
+        # write your code here
+        prefix_hash = {0: -1} #0 is the sum
+        prefix_sum = 0
 
+        count = len(nums)
+        for i in range(count):
+            prefix_sum += nums[i]
+            if prefix_sum in prefix_hash:
+                return prefix_hash[prefix_sum] + 1, i
+            else:
+                prefix_hash[prefix_sum] = i
+        
+        return None
+
+    
 public class Solution {
     /**
      * @param nums: A list of integers
