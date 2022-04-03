d44fc2454449f0a792642333fc8b30d090f829e1
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-02 19:01:38 -0700

    Update 0141.Linked_List_Cycle.java

--- Solutions/0141.Linked_List_Cycle.java
+++ Solutions/0141.Linked_List_Cycle.java
@@ -9,6 +9,26 @@ Output: true
 Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
 */
 
+# version python
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head or not head.next:
+            return False
+        
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            
+        return False
 
 //version1:hash
 /**
