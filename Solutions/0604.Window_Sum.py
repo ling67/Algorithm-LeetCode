c0ef8e2b11d654ef3277725099971f1409acaf12
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-03 15:22:09 -0700

    Update and rename 0604.Window_Sum.java to 0604.Window_Sum.py

--- Solutions/0604.Window_Sum.py
+++ Solutions/0604.Window_Sum.py
@@ -14,6 +14,25 @@ Explanation：
 
 */
 
+class Solution:
+    """
+    @param nums: a list of integers.
+    @param k: length of window.
+    @return: the sum of the element inside the window at each moving.
+    """
+    def win_sum(self, nums: List[int], k: int) -> List[int]:
+        if k == 0:
+            return []
+        n = len(nums)
+        sum = [0] * n
+        #计算前缀和
+        for i in range(1,n):
+            sum[i] = sum[i - 1] + nums[i]
+        res = []
+        k -= 1
+        for i in range(k,n):
+            res.append(sum[i] - sum[i - k] + nums[i-k])
+        return res
 
 public class Solution {
     /**
