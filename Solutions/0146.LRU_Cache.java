8de53af938329e0f3b0ed5f9d81e3b01c63e8175
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-26 16:15:31 -0700

    Update 0146.LRU_Cache.java

--- Solutions/0146.LRU_Cache.java
+++ Solutions/0146.LRU_Cache.java
@@ -32,6 +32,44 @@ lRUCache.get(4);    // return 4
 
 */
 
+
+
+#use a queue to store the time-linear data and dictionary to store the cache number, very straighr forward.
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.q = collections.deque()   #保存key,最recenty used放在最后面，最least used放在最前面
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        else:
+            self.q.remove(key)        #O(n)
+            self.q.append(key)
+            return self.cache[key]
+        
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:    # if key not in cache, put it in cache and q, and slao check the size of q
+            self.cache[key] = value
+            self.q.append(key)
+            if len(self.q) > self.capacity:
+                leastUsedKey = self.q.popleft()
+                del self.cache[leastUsedKey]
+        else:                       # if key is in cache, then renew it is corresponding value 
+            self.cache[key] = value
+            self.q.remove(key)     #O(N)
+            self.q.append(key)
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+ 
+
 /*
 approach1 extends LinkedHashMap
 approach2 use list and node
