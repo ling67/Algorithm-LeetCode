d14b2e58001e11c5aeecbf502a40dbbfad50c2de
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-27 15:41:33 -0700

    Update and rename 0128.Longest_Consecutive_Sequence.java to 0128.Longest_Consecutive_Sequence.py

--- Solutions/0128.Longest_Consecutive_Sequence.py
+++ Solutions/0128.Longest_Consecutive_Sequence.py
@@ -24,6 +24,25 @@ Output: 9
 3.hashset
 */
 
+//version3 python版本
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)
+        
+        for num in num_set:
+            if num - 1 not in num_set:
+                curr_num = num
+                curr_streak = 1
+                
+                while curr_num + 1 in num_set:
+                    curr_num += 1
+                    curr_streak += 1
+                    
+                longest_streak = max(longest_streak, curr_streak)
+        return longest_streak
+       
+
 //version3 有时间可以做下V1和V2
 class Solution {
     public int longestConsecutive(int[] nums) {
