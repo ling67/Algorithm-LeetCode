4d9139067275683670f547a0a2c6c1aa3ba8d006
ling67 <93315926+ling67@users.noreply.github.com>
2022-04-27 17:24:52 -0700

    Update 1249.Minimum_Remove_to_Make_Valid_Parentheses.py

--- Solutions/1249.Minimum_Remove_to_Make_Valid_Parentheses.py
+++ Solutions/1249.Minimum_Remove_to_Make_Valid_Parentheses.py
@@ -33,6 +33,30 @@ s[i] is either'(' , ')', or lowercase English letter.
 
 """
 
+//https://leetcode-cn.com/problems/minimum-remove-to-make-valid-parentheses/solution/yi-chu-wu-xiao-gua-hao-by-leetcode/
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        indexes_to_remove = set()
+        stack = []
+        for i, c in enumerate(s):
+            if c not in "()":
+                continue
+            if c == "(":
+                stack.append(i)
+            elif not stack:
+                indexes_to_remove.add(i)
+            else:
+                stack.pop()
+        indexes_to_remove = indexes_to_remove.union(set(stack))
+        string_builder = []
+        for i, c in enumerate(s):
+            if i not in indexes_to_remove:
+                string_builder.append(c)
+                
+        return "".join(string_builder)
+            
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         ct = 0
