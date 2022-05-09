c6c413ee31bcde0fb0ba042dd757ac95823856f5
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-08 18:22:35 -0700

    Update 0265.Paint_House_II.py

--- Solutions/0265.Paint_House_II.py
+++ Solutions/0265.Paint_House_II.py
@@ -31,15 +31,41 @@ costs[i].length == k
 """
 
 """
+python version 1
+1.define state dp[i][0] represent hourse 0...i-1 has painted and i paint by color 0
+2.get min{dp[n-1][0....k-1]}
+3.initialize dp[0][0]....dp[0][k-1] = cost[0][0]...cost[0][k-1]
+4.dp[i][j] = min{dp[i-1][0]...dp[i-1][j-1]...dp[i-1][j+1]...dp[i-1][k-1]} + cost[i][j]
+"""
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+        n, k = len(costs), len(costs[0])
+        dp = [[0] * k for _ in range(n)]
+        for i in range(k):
+            dp[0][i] = costs[0][i]
+        
+        for i in range(1, n):
+            for j in range(k):
+                minValue = float('inf')
+                for t in range(k):
+                    if t != j:
+                        minValue = min(minValue, dp[i-1][t])
+                dp[i][j] =  minValue + costs[i][j]
+                
+        return min(dp[-1])
+
+"""
+python version 2
 跟之前不一样的是变成了K个颜色
 
 优化成O(N*K)，因为每次都需要求出最小值，如果我们记录最小值就可以减小计算量了
 提前计算好的思想非常重要，后面的1289. Minimum Falling Path Sum II也会用到
-1.定义状态 dp[i][j] 代表第I+1栋房子，染色成j+1这个颜色后，最小的房子花费
-2.求 min dp[n-1][0...k]
-3.初始化 dp[0][0...k-1] = costs[0][0...k-1]
-4.递推公式
+1.define state dp[i][0] represent hourse 0...i-1 has painted and i paint by color 0
+2.get min{dp[n-1][0....k-1]}
+3.initialize dp[0][0]....dp[0][k-1] = cost[0][0]...cost[0][k-1]
+4.dp[i][j] = min{dp[i-1][0]...dp[i-1][j-1]...dp[i-1][j+1]...dp[i-1][k-1]} + cost[i][j]
 
+k*k*n
 """
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
