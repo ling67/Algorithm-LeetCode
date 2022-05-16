f3e9144e38dec32128900a609dac53ae306ee8d4
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-15 22:09:09 -0700

    Update 0075.Sort_Colors.py

--- Solutions/0075.Sort_Colors.py
+++ Solutions/0075.Sort_Colors.py
@@ -13,6 +13,34 @@ Output: [0,0,1,1,2,2]
 
 */
 
+//version 1：2次partition
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        lens = len(nums)
+        i, j = 0, lens - 1
+        while i <= j:
+            while i <= j and nums[i] == 0:
+                i += 1
+            while i <= j and nums[j] > 0:
+                j -= 1
+            if i <= j:
+                nums[i], nums[j] = nums[j], nums[i]
+        
+        i, j = i, lens - 1
+        while i <= j:
+            while i <= j and nums[i] == 1:
+                i += 1
+            while i <= j and nums[j] == 2:
+                j -= 1
+            if i <= j:
+                nums[i], nums[j] = nums[j], nums[i]
+        
+            
+            
+
 class Solution {
     public void sortColors(int[] nums) {
         int left = 0, right = nums.length - 1, index = 0;
