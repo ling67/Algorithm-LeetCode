adf97593e789550cd3f9f57e7fe7ee245606719b
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-16 11:21:07 -0700

    Update 0031.Partition_Array.py

--- Solutions/0031.Partition_Array.py
+++ Solutions/0031.Partition_Array.py
@@ -26,6 +26,32 @@ Explanation:
 Empty array, print 0.
 */
 
+class Solution:
+    """
+    @param nums: The integer array you should partition
+    @param k: An integer
+    @return: The index after partition
+    """
+    def partitionArray(self, nums, k):
+        # write your code here
+        lens = len(nums)
+        if lens == 0:
+            return 0
+
+        l, r = 0, lens - 1
+        while l <= r:
+            while l <= r and nums[l] < k:
+                l += 1
+            while l <= r and nums[r] >= k:
+                r -= 1
+            if l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        return l
+    
+
 public class Solution {
     /**
      * @param nums: The integer array you should partition
