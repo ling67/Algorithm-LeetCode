838c1a870ec99e814c5e7912d5afc669c7e63116
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-17 09:59:17 -0700

    Update 0215.Kth_Largest_Element_in_an_Array.py

--- Solutions/0215.Kth_Largest_Element_in_an_Array.py
+++ Solutions/0215.Kth_Largest_Element_in_an_Array.py
@@ -16,13 +16,43 @@ Output: 4
 
 */
 
-//python version
+//python heap
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         heapify(nums)    #初始化成最小堆
         while len(nums) > k:
             heappop(nums)
         return nums[0]
+       
+//python quick select
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        if not nums or k < 1 or k > len(nums):
+            return None
+        
+        return self.quick_select(nums, 0, len(nums) - 1, len(nums) - k)
+    
+    def quick_select(self, nums, start, end, k):
+        if start == end:    #模板注意点1，跟sort排序不一样
+            return nums[k]
+        
+        left, right = start, end
+        pivot = nums[(start + end) // 2]
+        while left <= right:    # 模板注意点2
+            while left <= right and nums[left] < pivot:    # 模板注意点3
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1 
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        if k <= right:      # 模板注意点4
+            return self._quick_select(nums, start, right, k)
+        elif k >= left:     # 模板注意点5
+            return self._quick_select(nums, left, end, k)
+        else:
+            return nums[k]
        
 //java version
 class Solution {
