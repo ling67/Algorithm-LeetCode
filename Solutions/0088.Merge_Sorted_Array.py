e854a2f995f0e51ae014031e80dc7d3c2a0a3914
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-17 14:32:50 -0700

    Update and rename 0088.Merge_Sorted_Array.java to 0088.Merge_Sorted_Array.py

--- Solutions/0088.Merge_Sorted_Array.py
+++ Solutions/0088.Merge_Sorted_Array.py
@@ -15,27 +15,6 @@ Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 */
 
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = nums1.length - 1;
-        int i = m -1, j = n - 1;
-        while ((i >= 0) && (j >= 0)) {
-            if (nums2[j] >= nums1[i]) {
-                nums1[index--] = nums2[j--];
-            } else {
-                nums1[index--] = nums1[i--];
-            }
-        }    
-        
-        while (i >= 0) {
-            nums1[index--] = nums1[i--];
-        }
-        while (j >= 0) {
-            nums1[index--] = nums2[j--];
-        }
-    }
-}
-
 //version:python
 
 class Solution:
@@ -68,4 +47,25 @@ class Solution:
             n -= 1
             index -= 1
 
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = nums1.length - 1;
+        int i = m -1, j = n - 1;
+        while ((i >= 0) && (j >= 0)) {
+            if (nums2[j] >= nums1[i]) {
+                nums1[index--] = nums2[j--];
+            } else {
+                nums1[index--] = nums1[i--];
+            }
+        }    
+        
+        while (i >= 0) {
+            nums1[index--] = nums1[i--];
+        }
+        while (j >= 0) {
+            nums1[index--] = nums2[j--];
+        }
+    }
+}
 
