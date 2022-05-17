67647cde09d01ee8ab0dc8adac6cb00787ad5109
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-16 17:58:35 -0700

    Update 0160.Intersection_of_Two_Linked_Lists.java

--- Solutions/0160.Intersection_of_Two_Linked_Lists.java
+++ Solutions/0160.Intersection_of_Two_Linked_Lists.java
@@ -16,8 +16,55 @@ Note that the linked lists must retain their original structure after the functi
 #         self.val = x
 #         self.next = None
 
-#version1：hashset 做很快
-# version2： connect head and tail
+#1.暴力解法
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        while headA is not None:
+            pB = headB
+            while pB is not None:
+                if headA == pB:
+                    return headA
+                pB = pB.next
+            headA = headA.next
+
+        return None
+
+#2.hashset   O(N+M) O(M)
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        nodes_in_B = set()
+
+        while headB is not None:
+            nodes_in_B.add(headB)
+            headB = headB.next
+
+        while headA is not None:
+            # if we find the node pointed to by headA,
+            # in our set containing nodes of B, then return the node
+            if headA in nodes_in_B:
+                return headA
+            headA = headA.next
+
+        return None
+ 
+ #3.Two point   O(N+M) O(1)
+ class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = headB if pA is None else pA.next
+            pB = headA if pB is None else pB.next
+
+        return pA
+        # Note: In the case lists do not intersect, the pointers for A and B
+        # will still line up in the 2nd iteration, just that here won't be
+        # a common node down the list and both will reach their respective ends
+        # at the same time. So pA will be NULL in that case.
+ 
+
+# 4.version connect head and tail 舍弃
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if not headA or not headA.next or not headB or not headB.next:
