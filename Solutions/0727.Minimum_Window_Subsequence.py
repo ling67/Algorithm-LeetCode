86fcb0784dc0f3754deb4475a21556bbd71f5e1a
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-18 18:57:45 -0700

    Update 0727.Minimum_Window_Subsequence.py

--- Solutions/0727.Minimum_Window_Subsequence.py
+++ Solutions/0727.Minimum_Window_Subsequence.py
@@ -23,29 +23,32 @@ Constraints:
 1 <= s1.length <= 2 * 104
 1 <= s2.length <= 100
 s1 and s2 consist of lowercase English letters.
-"""
 
-Given strings s1 and s2, return the minimum contiguous substring part of s1, so that s2 is a subsequence of the part.
-
-If there is no such window in s1 that covers all characters in s2, return the empty string "". If there are multiple such minimum-length windows, return the one with the left-most starting index.
-
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        min_lens = len(s)
+        res = ""
+        j = 0
+        for i in range(len(s)):     # O(N)
+            while j < len(s) and not self._is_subseq(s[i:j], t):
+                j += 1
+            
+            if j - i < min_lens and self._is_subseq(s[i:j], t):
+                min_lens = j - i
+                res = s[i:j]        # O(N) here could be optimized to O(1), see below solution
+        
+        return res
+    
+    def _is_subseq(self, s, t):       # taks O(M)
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                i += 1
+        return j == len(t)
  
-
-Example 1:
-
-Input: s1 = "abcdebdde", s2 = "bde"
-Output: "bcde"
-Explanation: 
-"bcde" is the answer because it occurs before "bdde" which has the same length.
-"deb" is not a smaller window because the elements of s2 in the window must occur in order.
-Example 2:
-
-Input: s1 = "jmeqksfrsdcmsiwvaovztaqenprpvnbstl", s2 = "u"
-Output: ""
+ 方法1：超时
+       
  
-
-Constraints:
-
-1 <= s1.length <= 2 * 104
-1 <= s2.length <= 100
-s1 and s2 consist of lowercase English letters.
