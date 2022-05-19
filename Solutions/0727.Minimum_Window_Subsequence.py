6b478f0ca93c4ec23ea3b4008864141ba959511b
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-18 20:48:24 -0700

    Update 0727.Minimum_Window_Subsequence.py

--- Solutions/0727.Minimum_Window_Subsequence.py
+++ Solutions/0727.Minimum_Window_Subsequence.py
@@ -24,6 +24,9 @@ Constraints:
 1 <= s2.length <= 100
 s1 and s2 consist of lowercase English letters.
 
+"""
+
+// 方法1：超时
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         min_lens = len(s)
@@ -49,6 +52,31 @@ class Solution:
                 i += 1
         return j == len(t)
  
- 方法1：超时
-       
+
+ """
+solution 3: dp
+dp[i][j] = the minimum window subsequence that ends with ith in s, and jth in t
+if s[i-1] == t[j-1]: dp[i][j] = 1 + dp[i-1][j-1]
+else: dp[i][j] = 1 + dp[i-1][j]
+"""
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        m, n = len(s), len(t)
+        dp = [[20000] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 0
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i-1] == t[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1 + dp[i-1][j]
+        
+        min_len = min(dp[i][n] for i in range(m + 1))
+        if min_len == 20000:
+            return ""
+        for i in range(m + 1):
+            if dp[i][n] == min_len:
+                return s[i-min_len:i]      
  
