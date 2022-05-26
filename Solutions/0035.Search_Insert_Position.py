a1dbe716a32918a71ec2501afcaea3419f6be79e
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-26 16:11:25 -0700

    Update and rename 0035.Search_Insert_Position.java to 0035.Search_Insert_Position.py

--- Solutions/0035.Search_Insert_Position.py
+++ Solutions/0035.Search_Insert_Position.py
@@ -19,6 +19,24 @@ Input: nums = [1,3,5,6], target = 7
 Output: 4
 */
 
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target <= nums[0]:
+            return 0
+        if target > nums[-1]:
+            return len(nums)
+        
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid
+            else:
+                start = mid
+        
+        return end
 
 class Solution {
     public int searchInsert(int[] nums, int target) {
