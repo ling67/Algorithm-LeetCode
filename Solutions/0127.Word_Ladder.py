442586a8a5d4e4b885025f312c57797d3fa16125
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-28 21:10:54 -0700

    Update 0127.Word_Ladder.py

--- Solutions/0127.Word_Ladder.py
+++ Solutions/0127.Word_Ladder.py
@@ -20,6 +20,43 @@ Output: 0
 Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
 */
 
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        word_set = set(wordList)
+        if endWord not in word_set:
+            return 0
+        
+        q = collections.deque()
+        visited = set()
+        q.append(beginWord)
+        visited.add(beginWord)
+        
+        steps = 0
+        while len(q) > 0:
+            steps += 1
+            lens = len(q)
+            for _ in range(lens):
+                curr_word = q.popleft()
+                if curr_word == endWord:
+                    return steps
+                for next_word in self._get_next(word_set, curr_word):
+                    if next_word not in visited:
+                        q.append(next_word)
+                        visited.add(next_word)
+        return 0
+    
+    def _get_next(self, word_set, curr_word):
+        res = []
+        for i, ch in enumerate(curr_word):
+            for letter in "abcdefghijklmnopqrstuvwxyz":
+                if letter == ch:
+                    continue
+                next_word = curr_word[:i] + letter + curr_word[i+1:]
+                if next_word in word_set:
+                    res.append(next_word)
+        return res
+       
+
 class Solution {
     
     //可以转换成起点到终点的最短路径  分层遍历三层循环 1.queue！= null 2.size = queue.size 当前这一层的每个节点 3.for循环出当前循环的每个节点的下一个单词
