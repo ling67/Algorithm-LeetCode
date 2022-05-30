ecb836754772f38673fc151e97cd3272ba4ed128
ling67 <93315926+ling67@users.noreply.github.com>
2022-05-29 23:32:14 -0700

    Update 2000.Sentence Reverse.py

--- Solutions/2000.Sentence Reverse.py
+++ Solutions/2000.Sentence Reverse.py
@@ -24,6 +24,16 @@ Constraints:
 [output] array.character
 """
 
+"""
+One possible solution is doing a linear iteration on arr while pushing a copy of every word to a stack and then pulling them in reverse order while copying the words back into arr. Another approach is initializing a new array at the same length, iterating over arr from right to left and copying any sequence of characters to the new array from left to right. Both approaches take O(N) time and at least O(N) space.
+
+A more elegant and efficient approach is to reverse all the characters in arr and then reverse the characters in each word separately. While the first reverse gives us the words in the reverse order as we wanted, it also reverses the characters of each word. To fix that, we do the second reverse, which reverses each word separately.
+
+Reversing items in an array is done by a ‘mirror’ function, that swaps the items of every 2 indices with the same distance from the middle.
+"""
+
+
+
 def reverse_helper(arr, start, end):
     while start < end:
       arr[start], arr[end] = arr[end], arr[start]
