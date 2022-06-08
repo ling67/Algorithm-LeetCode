7f7744c18f07dda54664b53220bbeffa37f76c5e
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-08 16:18:34 -0700

    Create 0115.Distinct_Subsequences.py

---
+++ Solutions/0115.Distinct_Subsequences.py
@@ -0,0 +1,60 @@
+"""
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+A string's subsequence is a new string formed from the original string by deleting some (can be none) of the characters without disturbing the remaining characters' relative positions. (i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from S.
+rabbbit
+rabbbit
+rabbbit
+Example 2:
+
+Input: s = "babgbag", t = "bag"
+Output: 5
+Explanation:
+As shown below, there are 5 ways you can generate "bag" from S.
+babgbag
+babgbag
+babgbag
+babgbag
+babgbag
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 1000
+s and t consist of English letters.
+"""
+
+"""
+# dp[i][j] = the number of discinct subeseq until ith char in s and jth char in t
+# if s[i]!=t[j], dp[i][j] = dp[i - 1][j]  eg: rab, ra
+# else: rabb, rab, dp[i][j] = dp[i-1][j] + dp[i-1][j-1], 品，细品！
+"""
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+        if m < n:
+            return 0
+        
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            dp[i][0] = 1
+            
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                dp[i][j] = dp[i-1][j]
+                if s[i-1] == t[j-1]:
+                    dp[i][j] += dp[i-1][j-1] 
+                    
+        return dp[m][n]
+
