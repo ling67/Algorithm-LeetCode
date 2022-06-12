932798ba4f5e8ba8098bb3d70eea7b6d5705822d
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-12 09:18:47 -0700

    Update 1110.Delete_Nodes_And_Return_Forest.py

--- Solutions/1110.Delete_Nodes_And_Return_Forest.py
+++ Solutions/1110.Delete_Nodes_And_Return_Forest.py
@@ -33,35 +33,39 @@ if a node is in to_delete list, then the node should pass the information to it'
 deleted and it's children has no parent now.
 """
 
-"""
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        self.res = []
+        set_delete = set(to_delete)
+        ans = self.process(root, set_delete)
+        if ans:
+            self.res.append(ans)
+        return self.res
+    
+    def process(self, root, set_delete):
+        if not root:
+            return None
+        
+        root.left = self.process(root.left, set_delete)
+        root.right = self.process(root.right, set_delete)
+        
+        if root.val not in set_delete:
+            return root
+        
+        if root.left:
+            self.res.append(root.left)
+        if root.right:
+            self.res.append(root.right)
+            
+        return None
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
-        ans = []
-        ds = set(to_delete)   # search more efficient
-        
-        def process(root):
-            if not root:
-                return None
-            root.left, root.right = process(root.left), process(root.right)  #if root.left root.right == none means they are both processed
-            if root.val not in to_delete:
-                return root    #can contact with parent
-            # in this step indicate root in to_delete, so we need add left or right to ans
-            if root.left:
-                ans.append(root.left)
-            if root.right:
-                ans.append(root.right)
-            return None
-        
-        root = process(root)
-        if root:
-            ans.append(root)
-            
-        return ans
-    
+
+
+
 
