aa14d8d0feefb2ea54531f7b39b9c94287b3308e
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-12 18:35:28 -0700

    Create 0894.All_Possible_Full_Binary_Trees.py

---
+++ Solutions/0894.All_Possible_Full_Binary_Trees.py
@@ -0,0 +1,50 @@
+"""
+Given an integer n, return a list of all possible full binary trees with n nodes. Each node of each tree in the answer must have Node.val == 0.
+
+Each element of the answer is the root node of one possible tree. You may return the final list of trees in any order.
+
+A full binary tree is a binary tree where each node has exactly 0 or 2 children.
+
+ 
+
+Example 1:
+
+
+Input: n = 7
+Output: [[0,0,0,null,null,0,0,null,null,0,0],[0,0,0,null,null,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,null,null,null,null,0,0],[0,0,0,0,0,null,null,0,0]]
+Example 2:
+
+Input: n = 3
+Output: [[0,0,0]]
+ 
+
+Constraints:
+
+1 <= n <= 20
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        if N % 2 != 1:    # only odd number of nodes can form a full binary tree
+            return []
+        if N == 1:
+            return [TreeNode(0)]
+        
+        res = []
+        for i in range(1, N, 2):
+            left_roots = self.allPossibleFBT(i)
+            right_roots = self.allPossibleFBT(N-i-1)
+            for left_node in left_roots:
+                for right_node in right_roots:
+                    root = TreeNode(0)
+                    root.left = left_node
+                    root.right = right_node
+                    res.append(root)
+            
+        return res
