7cea8aac2110c8ba94475a7c40170df44c211796
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-21 13:28:24 -0700

    Create 1514.Path_with_Maximum_Probability.py

---
+++ Solutions/1514.Path_with_Maximum_Probability.py
@@ -0,0 +1,69 @@
+"""
+You are given an undirected weighted graph of n nodes (0-indexed), represented by an edge list where edges[i] = [a, b] is an undirected edge connecting the nodes a and b with a probability of success of traversing that edge succProb[i].
+
+Given two nodes start and end, find the path with the maximum probability of success to go from start to end and return its success probability.
+
+If there is no path from start to end, return 0. Your answer will be accepted if it differs from the correct answer by at most 1e-5.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.2], start = 0, end = 2
+Output: 0.25000
+Explanation: There are two paths from start to end, one having a probability of success = 0.2 and the other has 0.5 * 0.5 = 0.25.
+Example 2:
+
+
+
+Input: n = 3, edges = [[0,1],[1,2],[0,2]], succProb = [0.5,0.5,0.3], start = 0, end = 2
+Output: 0.30000
+Example 3:
+
+
+
+Input: n = 3, edges = [[0,1]], succProb = [0.5], start = 0, end = 2
+Output: 0.00000
+Explanation: There is no path between 0 and 2.
+ 
+
+Constraints:
+
+2 <= n <= 10^4
+0 <= start, end < n
+start != end
+0 <= a, b < n
+a != b
+0 <= succProb.length == edges.length <= 2*10^4
+0 <= succProb[i] <= 1
+There is at most one edge between every two nodes.
+"""
+
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        graph = collections.defaultdict(list)
+        for i, (u, v) in enumerate(edges):
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
+            
+        #求最大值，就是求负的最小值
+        hq = [(-1, start)] #
+        costs = defaultdict(int) 
+        
+        while hq:
+            curr_prob, curr_node = heappop(hq)
+            curr_prob = -curr_prob
+            
+            if curr_node == end:
+                return curr_prob
+            
+            if curr_node in costs:
+                continue
+            costs[curr_node] = curr_prob
+            
+            for next_node, next_cost in graph[curr_node]:
+                heappush(hq, (-curr_prob * next_cost, next_node))
+        
+        return 0.0
