03e7697fbce0559f1f4043ea84c0f53ad000873b
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-22 14:48:52 -0700

    Create 0547.Number_of_Provinces.py

---
+++ Solutions/0547.Number_of_Provinces.py
@@ -0,0 +1,63 @@
+"""
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+Example 2:
+
+
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+ 
+
+Constraints:
+
+1 <= n <= 200
+n == isConnected.length
+n == isConnected[i].length
+isConnected[i][j] is 1 or 0.
+isConnected[i][i] == 1
+isConnected[i][j] == isConnected[j][i]
+"""
+
+class UnionFind:
+    def __init__(self, n):
+        self.father = collections.defaultdict(int)
+        self.cnt = n 
+        for i in range(n):
+            self.father[i] = i
+    
+    def find(self, a):
+        if self.father[a] == a:
+            return a
+        else:
+            self.father[a] = self.find(self.father[a])
+            return self.father[a]
+    
+    def connect(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a != root_b:
+            self.father[root_a] = root_b
+            self.cnt -= 1
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        uf = UnionFind(n)
+        for i in range(n):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1:
+                    uf.connect(i, j)
+        return uf.cnt
