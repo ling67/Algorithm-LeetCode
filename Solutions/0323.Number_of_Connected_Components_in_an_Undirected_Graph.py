1d83356737f1e303c8067e754ceeca919741e7cd
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-22 11:33:00 -0700

    Create 0323.Number_of_Connected_Components_in_an_Undirected_Graph.py

---
+++ Solutions/0323.Number_of_Connected_Components_in_an_Undirected_Graph.py
@@ -0,0 +1,59 @@
+"""
+You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
+
+ 
+
+Example 1:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[3,4]]
+Output: 2
+Example 2:
+
+
+Input: n = 5, edges = [[0,1],[1,2],[2,3],[3,4]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= n <= 2000
+1 <= edges.length <= 5000
+edges[i].length == 2
+0 <= ai <= bi < n
+ai != bi
+There are no repeated edges.
+"""
+
+
+class UnionFind:
+    def __init__(self, n):
+        self.father = collections.defaultdict(int)
+        for i in range(n):
+            self.father[i] = i
+        self.cnt = n
+    
+    def find(self, a):
+        if self.father[a] == a:
+            return a
+        self.father[a] = self.find(self.father[a])
+        return self.father[a]
+    
+    def connect(self, a, b):
+        father_a = self.find(a)
+        father_b = self.find(b)
+        if father_a != father_b:
+            self.father[father_a] = father_b
+            self.cnt -= 1
+    
+    def query(self):
+        return self.cnt
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        uf = UnionFind(n)
+        for u, v in edges:
+            uf.connect(u, v)
+        return uf.query()
