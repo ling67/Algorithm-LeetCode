eed04e78f9df7acd38e3516200f0b8c0027acbb1
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-23 10:42:44 -0700

    Create 0399.Evaluate_Division.py

---
+++ Solutions/0399.Evaluate_Division.py
@@ -0,0 +1,75 @@
+"""
+You are given an array of variable pairs equations and an array of real numbers values, where equations[i] = [Ai, Bi] and values[i] represent the equation Ai / Bi = values[i]. Each Ai or Bi is a string that represents a single variable.
+
+You are also given some queries, where queries[j] = [Cj, Dj] represents the jth query where you must find the answer for Cj / Dj = ?.
+
+Return the answers to all queries. If a single answer cannot be determined, return -1.0.
+
+Note: The input is always valid. You may assume that evaluating the queries will not result in division by zero and that there is no contradiction.
+
+ 
+
+Example 1:
+
+Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0], queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
+Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
+Explanation: 
+Given: a / b = 2.0, b / c = 3.0
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ?
+return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+Example 2:
+
+Input: equations = [["a","b"],["b","c"],["bc","cd"]], values = [1.5,2.5,5.0], queries = [["a","c"],["c","b"],["bc","cd"],["cd","bc"]]
+Output: [3.75000,0.40000,5.00000,0.20000]
+Example 3:
+
+Input: equations = [["a","b"]], values = [0.5], queries = [["a","b"],["b","a"],["a","c"],["x","y"]]
+Output: [0.50000,2.00000,-1.00000,-1.00000]
+ 
+
+Constraints:
+
+1 <= equations.length <= 20
+equations[i].length == 2
+1 <= Ai.length, Bi.length <= 5
+values.length == equations.length
+0.0 < values[i] <= 20.0
+1 <= queries.length <= 20
+queries[i].length == 2
+1 <= Cj.length, Dj.length <= 5
+Ai, Bi, Cj, Dj consist of lower case English letters and digits.
+"""
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def dfs(curr_node, end, curr_div):
+            if curr_node == end:
+                self.div = curr_div   #can not directly append to res, bcz if we donot find it we need append -1
+                return
+            for next_node, div in graph[curr_node]:
+                if next_node not in visited:
+                    next_div = div * curr_div
+                    visited.add(next_node)
+                    dfs(next_node, end, next_div)
+        
+        graph = defaultdict(list)
+        for i, (u, v) in enumerate(equations):
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1 / values[i]))
+        
+        mutiple = 1
+        
+        res = []
+        for start, end in queries:
+            if start not in graph or end not in graph:
+                res.append(-1.0)
+                continue
+            if start == end:
+                res.append(1.0)
+                continue
+            self.div = -1   #record each time div
+            visited = set()
+            visited.add(start)
+            dfs(start, end, 1)
+            res.append(self.div)
+        return res
