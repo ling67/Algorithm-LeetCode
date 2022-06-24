a4d1328b5c6fb3185066085074e575778e92b74b
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-23 17:30:16 -0700

    Update 0733.Flood_Fill.py

--- Solutions/0733.Flood_Fill.py
+++ Solutions/0733.Flood_Fill.py
@@ -36,19 +36,18 @@ n == image[i].length
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         def dfs(curr_i, curr_j):
+            visited.add((curr_i, curr_j))
+            image[curr_i][curr_j] = color
+
             for delta_i, delta_j in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
                 next_i, next_j = curr_i + delta_i, curr_j + delta_j
                 if 0 <= next_i < m and 0 <= next_j < n and image[next_i][next_j] == start_color:
                     if (next_i, next_j) not in visited:
-                        image[next_i][next_j] = color
-                        visited.add((next_i, next_j))
                         dfs(next_i, next_j)        
                     
         m, n = len(image), len(image[0])            
         visited = set()
-        visited.add((sr, sc))
         start_color = image[sr][sc]
-        image[sr][sc] = color
         dfs(sr, sc)
         return image
 
