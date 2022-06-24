8561371123d39e9fb87c13fee409ee3271a1e0ee
ling67 <93315926+ling67@users.noreply.github.com>
2022-06-23 19:55:38 -0700

    Create 0077.Combinations.py

---
+++ Solutions/0077.Combinations.py
@@ -0,0 +1,44 @@
+"""
+Given two integers n and k, return all possible combinations of k numbers out of the range [1, n].
+
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+Example 2:
+
+Input: n = 1, k = 1
+Output: [[1]]
+ 
+
+Constraints:
+
+1 <= n <= 20
+1 <= k <= n
+"""
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(curr_idx, curr_comb):
+            if len(curr_comb) == k:
+                res.append(curr_comb.copy())
+            for next_idx in range(curr_idx + 1, n + 1):
+                curr_comb.append(next_idx)
+                backtrack(next_idx, curr_comb)
+                curr_comb.pop()
+        res = []
+        backtrack(0, [])
+        return res
+
