def5e8ca4f7d38fac99a0f7537e6efc7302cf735
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-08 21:20:59 -0700

    Update 0941.Valid_Mountain_Array.py

--- Solutions/0941.Valid_Mountain_Array.py
+++ Solutions/0941.Valid_Mountain_Array.py
@@ -35,6 +35,20 @@ class Solution:
         if len(arr) < 3:
             return False
 
+        i, j = 0, len(arr) - 1
+        while i < len(arr) - 1 and arr[i] < arr[i + 1]:
+            i += 1
+        while j >= 0 and arr[j] < arr[j - 1]:
+            j -= 1
+        
+        return i == j and i != 0 and j != len(arr) - 1
+        
+            
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if len(arr) < 3:
+            return False
+
         inc_cnt = 0
         dec_cnt = 0
         for i in range(1, len(arr)):
