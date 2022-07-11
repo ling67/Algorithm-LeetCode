d184d3c11f3c7062f2d013729dd87e3ef4245081
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-11 11:33:54 -0700

    Create 1482.Minimum_Number_of_Days_to_Make_m_Bouquets.py

---
+++ Solutions/1482.Minimum_Number_of_Days_to_Make_m_Bouquets.py
@@ -0,0 +1,80 @@
+"""
+You are given an integer array bloomDay, an integer m and an integer k.
+
+You want to make m bouquets. To make a bouquet, you need to use k adjacent flowers from the garden.
+
+The garden consists of n flowers, the ith flower will bloom in the bloomDay[i] and then can be used in exactly one bouquet.
+
+Return the minimum number of days you need to wait to be able to make m bouquets from the garden. If it is impossible to make m bouquets return -1.
+
+ 
+
+Example 1:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 1
+Output: 3
+Explanation: Let us see what happened in the first three days. x means flower bloomed and _ means flower did not bloom in the garden.
+We need 3 bouquets each should contain 1 flower.
+After day 1: [x, _, _, _, _]   // we can only make one bouquet.
+After day 2: [x, _, _, _, x]   // we can only make two bouquets.
+After day 3: [x, _, x, _, x]   // we can make 3 bouquets. The answer is 3.
+Example 2:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 2
+Output: -1
+Explanation: We need 3 bouquets each has 2 flowers, that means we need 6 flowers. We only have 5 flowers so it is impossible to get the needed bouquets and we return -1.
+Example 3:
+
+Input: bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+Output: 12
+Explanation: We need 2 bouquets each should have 3 flowers.
+Here is the garden after the 7 and 12 days:
+After day 7: [x, x, x, x, _, x, x]
+We can make one bouquet of the first three flowers that bloomed. We cannot make another bouquet from the last three flowers that bloomed because they are not adjacent.
+After day 12: [x, x, x, x, x, x, x]
+It is obvious that we can make two bouquets in different ways.
+ 
+
+Constraints:
+
+bloomDay.length == n
+1 <= n <= 105
+1 <= bloomDay[i] <= 109
+1 <= m <= 106
+1 <= k <= n
+
+"""
+
+
+"""
+花束：m, 每个花束里面有k枝花，但是这k枝花要相邻，否则不能做成花束。
+"""
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        start, end = min(bloomDay), max(bloomDay)
+        while start + 1 < end:
+            mid = start + (end - start) // 2
+            if self.can_make(bloomDay, mid, m, k):
+                end = mid
+            else:
+                start = mid
+        
+        if self.can_make(bloomDay, start, m, k):
+            return start
+        if self.can_make(bloomDay, end, m, k):
+            return end
+        
+        return -1
+    
+    def can_make(self, bloomDay, days, m, k):
+        cnt = 0
+        curr_adj = 0
+        for day in bloomDay:
+            if day <= days:
+                curr_adj += 1
+                if curr_adj >= k:
+                    cnt += 1
+                    curr_adj = 0
+            else:
+                curr_adj = 0
+        return cnt >= m
