de2a6812ffbeec4b8ccac1c8450dcd13add01fef
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-15 15:13:50 -0700

    Create 1021.Remove_Outermost_Parentheses.py

---
+++ Solutions/1021.Remove_Outermost_Parentheses.py
@@ -0,0 +1,62 @@
+"""
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+ 
+
+Example 1:
+
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+Example 3:
+
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either '(' or ')'.
+s is a valid parentheses string.
+"""
+
+
+"""
+只有2种情况要跳过： open记录左括号比右括号多的数量
+（且open = 1
+）且open = 0
+"""
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        opened = 0
+        res = ""
+        for ch in s:
+            if ch == '(':
+                opened += 1
+                if opened != 1:
+                    res += ch
+            else:
+                opened -= 1
+                if opened != 0:
+                    res += ch
+        return res
