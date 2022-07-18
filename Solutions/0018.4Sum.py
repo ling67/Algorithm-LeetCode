9888f775c5dbc67ba6be6216ef6018c9602017f1
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-17 21:23:32 -0700

    Update 0018.4Sum.py

--- Solutions/0018.4Sum.py
+++ Solutions/0018.4Sum.py
@@ -69,3 +69,36 @@ class Solution:
                         while left < right and nums[right] == nums[right + 1]:  #去重
                             right -= 1
         return res
+
+# Ksum 的通用写法
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res, quad = [], []
+        
+        def kSum(k, start, target):
+            if k != 2:
+                for i in range(start, len(nums) - k + 1):
+                    if i > start and nums[i] == nums[i - 1]:
+                        continue
+                    quad.append(nums[i])
+                    kSum(k - 1, i + 1, target - nums[i])
+                    quad.pop()   #好像就是backtrack
+                return 
+            
+            l, r = start, len(nums) - 1
+            while l < r:
+                if nums[l] + nums[r] < target:
+                    l += 1
+                elif nums[l] + nums[r] > target:
+                    r -= 1
+                else:
+                    res.append(quad + [nums[l], nums[r]])
+                    l += 1        
+                    while l < r and nums[l] == nums[l - 1]:   #因为上面一行没有移动r，所以这里也不需要对r去重
+                        l += 1
+        kSum(4, 0, target)
+        return res
+            
+       
