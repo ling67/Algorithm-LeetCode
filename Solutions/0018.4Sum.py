4396eb6aa0dfa8b3748c174c0350cf08e42c200b
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-17 21:26:16 -0700

    Update 0018.4Sum.py

--- Solutions/0018.4Sum.py
+++ Solutions/0018.4Sum.py
@@ -25,8 +25,67 @@ Constraints:
 -109 <= target <= 109
 """
 
-"""for循环nums[i]，然后for循环nums[j]，在用双指针解决twoSum问题 O(N^3), O(1)"""
+# Solution 1: HashSet 对结果去重
+"""
+The idea is to use HashSet to track past elements.
+We iterate the combinations of nums[i], nums[j], nums[k], and calculate the last number by lastNumber = target - nums[i] - nums[j] - nums[k].
+We check if lastNumber is existed the past by checking in the HashSet, if existed, then it form a quadruplets then add it to the answer.
+Complexity:
+Time: O(N^3)
+Extra Space (Without count output as space): O(N)
+"""
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        n = len(nums)
+        seen = set()
+        ans = set()
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    lastNumber = target - nums[i] - nums[j] - nums[k]
+                    if lastNumber in seen:
+                        arr = sorted([nums[i], nums[j], nums[k], lastNumber])
+                        ans.add((arr[0], arr[1], arr[2], arr[3]))
+            seen.add(nums[i])
+        return ans
 
+# Solution 2: two pointer  对结果去重
+"""
+Sort nums in increasing order.
+We fix nums[i], nums[j] by iterating the combination of nums[i], nums[j], then the problem now become to very classic problem 1. Two Sum.
+By using two pointers, one points to left, the other points to right, remain = target - nums[i] - nums[j].
+If nums[left] + nums[right] == remain Found a valid quadruplets
+Else if nums[left] + nums[right] > remain  Sum is bigger than remain, need to decrease sum by right -= 1
+Else: Increasing sum by left += 1.
+
+Complexity:
+Time: O(N^3)
+Extra Space (Without count output as space): O(sorting)
+"""
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        ans = set()
+        for i in range(n):
+            for j in range(i+1, n):
+                l, r = j + 1, n - 1
+                remain = target - nums[i] - nums[j]
+                while l < r:
+                    if nums[l] + nums[r] == remain:
+                        ans.add((nums[i], nums[j], nums[l], nums[r]))
+                        l += 1
+                        r -= 1
+                    elif nums[l] + nums[r] > remain:
+                        r -= 1
+                    else:
+                        l += 1
+        return ans
+
+
+# Solution 3: two pointer  对过程去重
+
+"""for循环nums[i]，然后for循环nums[j]，在用双指针解决twoSum问题 O(N^3), O(1)"""
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         if not nums or len(nums) < 4:
@@ -70,7 +129,7 @@ class Solution:
                             right -= 1
         return res
 
-# Ksum 的通用写法
+# Solution 4: Ksum 的通用写法
 
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
