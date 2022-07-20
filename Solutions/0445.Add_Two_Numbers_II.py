abb05a44b6948d7e768648ceae1168ee1a34220b
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-20 14:13:08 -0700

    Update and rename 0445.Add_Two_Numbers_II.java to 0445.Add_Two_Numbers_II.py

--- Solutions/0445.Add_Two_Numbers_II.py
+++ Solutions/0445.Add_Two_Numbers_II.py
@@ -50,3 +50,36 @@ class Solution:
             head.next = ListNode(int(nums[i]))
             head = head.next
         return dummy.next
+
+
+# Solution 2: use stack to solve the problem
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        s1 = []
+        s2 = []
+        while l1:
+            s1.append(l1.val)
+            l1 = l1.next
+        while l2:
+            s2.append(l2.val)
+            l2 = l2.next
+        temp = 0
+        res = ListNode(0)
+        while len(s1) != 0 or len(s2) != 0:
+            if len(s1) != 0:
+                temp += s1.pop()
+            if len(s2) != 0:
+                temp += s2.pop()
+            res.val = temp % 10
+            head = ListNode(temp // 10)
+            head.next = res
+            res = head
+            temp //= 10
+        return res.next if res.val == 0 else res
+        
+            
