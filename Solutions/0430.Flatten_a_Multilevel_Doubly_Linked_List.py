16d7758213a754e7804905ddb75be4438f021826
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-20 10:40:02 -0700

    Update 0430.Flatten_a_Multilevel_Doubly_Linked_List.py

--- Solutions/0430.Flatten_a_Multilevel_Doubly_Linked_List.py
+++ Solutions/0430.Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -72,6 +72,39 @@ class Node:
         self.child = child
 """
 
+# Solution 1: use recursion
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return
+        dummy = Node(0, None, head, None)
+        self.flattern_dfs(dummy, head)
+        dummy.next.prev = None
+        return dummy.next
+    
+    def flattern_dfs(self, prev, curr):
+        if not curr:
+            return prev
+        
+        curr.prev = prev
+        prev.next = curr
+        
+        tempNext = curr.next
+        tail = self.flattern_dfs(curr, curr.child)
+        curr.child = None
+        return self.flattern_dfs(tail, tempNext)
+            
+# Solution 2: use stack
 class Solution:
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
