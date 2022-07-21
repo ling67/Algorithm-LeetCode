822d16b8db4d278d2da6097f9b5840529c83d05c
ling67 <93315926+ling67@users.noreply.github.com>
2022-07-20 23:24:40 -0700

    Update 0462.Minimum_Moves_to_Equal_Array_Elements_II.py

--- Solutions/0462.Minimum_Moves_to_Equal_Array_Elements_II.py
+++ Solutions/0462.Minimum_Moves_to_Equal_Array_Elements_II.py
@@ -26,12 +26,61 @@ n == nums.length
 1 <= nums.length <= 105
 -109 <= nums[i] <= 109
 """
+"""
+先看只有两个点A和B的情况,
+______A_____P_______B_______
+可以发现，只要选择位置P在 [A, B] 区间内，不管在哪，距离之和都是A和B之间的距离，如果P不在 [A, B] 之间，那么距离之和就会大于A和B之间的距离，
+现在再加两个点C和D：
+______C_____A_____P_______B______D______
+通过分析可以得出，P点的最佳位置就是在 [A, B] 区间内.
+如果加入很多点，我们选择最佳的点是最小区间[A, B]之间的点，也就是[median - 1, median + 1]之间选一个点，
+这就是为什么我们需要选择median了
+"""
 
+"""
+solution 1: find median by sorting
+"""
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        median = self.find_median(nums)
+        median = self._find_median(nums)
         return sum(abs(num - median) for num in nums)
     
-    def find_median(self, nums):
+    def _find_median(self, nums):
         nums.sort()
-        return nums[len(nums) // 2]
+        return nums[len(nums)//2]
+        
+        
+"""
+solution 2: find meddian by quick select(kth largest element) - O(N)
+模板一个星期不看就不记得了
+"""
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        median = self._find_median(nums)
+        return sum(abs(num - median) for num in nums)
+    
+    def _find_median(self, nums):
+        return self._quick_select(nums, 0, len(nums) - 1, len(nums) // 2)
+        
+    def _quick_select(self, nums, start, end, k):
+        if start >= end:
+            return nums[k]
+        
+        pivot = nums[(start + end) // 2]
+        left, right = start, end
+        while left <= right:
+            while left <= right and nums[left] < pivot:
+                left += 1
+            while left <= right and nums[right] > pivot:
+                right -= 1
+            if left <= right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        
+        if k <= right:
+            return self._quick_select(nums, start, right, k)
+        elif k >= left:
+            return self._quick_select(nums, left, end, k)
+        
+        return nums[k]
