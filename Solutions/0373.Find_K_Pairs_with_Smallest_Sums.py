6237b6138bce589109c66cd7c320895f9dc8f4ca
ling67 <93315926+ling67@users.noreply.github.com>
2022-08-03 21:47:55 -0700

    Create 0373.Find_K_Pairs_with_Smallest_Sums.py

---
+++ Solutions/0373.Find_K_Pairs_with_Smallest_Sums.py
@@ -0,0 +1,55 @@
+"""
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+Example 3:
+
+Input: nums1 = [1,2], nums2 = [3], k = 3
+Output: [[1,3],[2,3]]
+Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+-109 <= nums1[i], nums2[i] <= 109
+nums1 and nums2 both are sorted in ascending order.
+1 <= k <= 104
+"""
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        hq = []
+        heappush(hq, (nums1[0] + nums2[0], 0, 0))
+        visited = set()
+        visited.add((0, 0))
+        
+        res = []
+        m, n = len(nums1), len(nums2)
+        for _ in range(min(m*n, k)):
+            curr, i, j = heappop(hq)
+            res.append([nums1[i], nums2[j]])
+            if i + 1 < m and (i + 1, j) not in visited:
+                heappush(hq, (nums1[i + 1] + nums2[j], i + 1, j))
+                visited.add((i + 1, j))
+            if j + 1 < n and (i, j + 1) not in visited:
+                heappush(hq, (nums1[i] + nums2[j + 1], i, j + 1))
+                visited.add((i, j + 1))
+        return res
+
+
