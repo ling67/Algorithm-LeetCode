8f6e1e874e950f5ac03eca69e02cacd97f7d281a
ling67 <93315926+ling67@users.noreply.github.com>
2023-01-16 21:44:16 -0800

    Update 0076.Minimum_Window_Substring.py

--- Solutions/0076.Minimum_Window_Substring.py
+++ Solutions/0076.Minimum_Window_Substring.py
@@ -39,61 +39,32 @@ time complexity, O(mn) m is the time to check is_valid()
 """
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        cnter_t = Counter(t)    #store string t ch_num
-        cnter = defaultdict(int)    #store string 
-        min_len = float("inf")
-        start, end = 0, 0  #record the index of min_len subarray.
-        j = 0
-        
-        #套用九章的模板 for at least problems
-        for i, ch in enumerate(s): 
-            while j < len(s) and not self.is_valid(cnter, cnter_t):  # s = "ADOBECODEBANC", t = "ABC" , j will became O.
-                cnter[s[j]] += 1
+        t_ch_cnt = Counter(t)  # record the character:freq of t
+        window_ch_cnt = defaultdict(int) # record the character:freq of t
+        min_len = sys.maxsize
+
+        i, j = 0, 0
+        start, end = 0, 0
+
+        for i, ch in enumerate(s):
+            while j < len(s) and not self.is_valid(window_ch_cnt, t_ch_cnt):
+                window_ch_cnt[s[j]] += 1
                 j += 1
-            
-            if self.is_valid(cnter, cnter_t):
+
+            if self.is_valid(window_ch_cnt, t_ch_cnt):
                 if j - i < min_len:
                     min_len = j - i
                     start, end = i, j
-            cnter[s[i]] -= 1
-            
-        return s[start:end]  #not include end
-    
+
+            window_ch_cnt[ch] -= 1
+            if window_ch_cnt[ch] == 0:
+                del window_ch_cnt[ch]
+
+        return s[start:end]
+
     #check if all character cnter_t has already in cnter_s
-    def is_valid(self, cnter_s, cnter_t):
-        for ch, cnt in cnter_t.items():
-            if cnt > cnter_s[ch]:
+    def is_valid(self, window_ch_cnt, t_ch_cnt):
+        for ch, cnt in t_ch_cnt.items():
+            if cnt > window_ch_cnt[ch]:
                 return False
         return True
-     
-    
-"""
-this solution is O(N), instead of using self.allIncluded(sourceDict, targetDict) to check matched or not, 
-we use a int missing to keep track of how many chars are still needed in order to match
-also, in stead of using s[i:j] everytime we renew res, we use start, end to renew the idx, which reduce time from O(N) to O(1)
-"""
-class Solution:
-    def minWindow(self, s: str, t: str) -> str:
-        cnter_t = Counter(t)
-        cnter = defaultdict(int)
-        missing = len(cnter_t)    # how many ch are still missing in order to match t
-        min_len = sys.maxsize
-        start, end = 0, 0
-        j = 0
-        for i, ch in enumerate(s):
-            while j < len(s) and missing > 0:
-                cnter[s[j]] += 1
-                if cnter[s[j]] == cnter_t[s[j]]:    # 正好加到满足条件了，就需要missing -= 1
-                    missing -= 1
-                j += 1
-            
-            if missing == 0:            # missing == 0 means matched
-                if j - i < min_len:
-                    min_len = j - i
-                    start, end = i, j   # 也可以定义一个res, 用res = s[i:j]来更新结果，但是takes O(N)时间，所以用start, end来记录就可以了
-            
-            cnter[s[i]] -= 1
-            if cnter[s[i]] == cnter_t[s[i]] - 1:    # 正好减到不满足条件了，就需要missing += 1
-                missing += 1
-                
-        return s[start:end]    
