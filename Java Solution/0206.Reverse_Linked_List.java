588ab7d7c8faf96546af75ccbe2a6cb0d172305b
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-07 15:26:26 -0800

    Create 0206.Reverse_Linked_List.java

---
+++ Java Solution/0206.Reverse_Linked_List.java
@@ -0,0 +1,24 @@
+//Solution: recursion
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode newHead = new ListNode(-1);
+        while (head != null) {
+            ListNode curr = head;
+            head = head.next;
+            curr.next = newHead.next;
+            newHead.next = curr;
+        }
+        return newHead.next;
+    }
+}
