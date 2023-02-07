068319ef2b32ede713fa77455609eaeb1638fb7b
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-06 17:56:18 -0800

    Update 0075.Sort_Colors.py

--- Solutions/0075.Sort_Colors.py
+++ Solutions/0075.Sort_Colors.py
@@ -62,31 +62,4 @@ class Solution:
                 nums[i], nums[j] = nums[j], nums[i]
         
             
-/*********************************************java version******************************************/        
-
-class Solution {
-    public void sortColors(int[] nums) {
-        int left = 0, right = nums.length - 1, index = 0;
-        //left的左边都是0，right的右边都是2
-        while (index <= right) {
-            if (nums[index] == 0) {
-                //交换
-                int temp = nums[index];
-                nums[index] = nums[left];
-                nums[left] = temp;
-                index++;
-                left++;
-            } else if(nums[index] == 1) {
-                index++;
-            } else {
-                //交换
-                int temp = nums[index];
-                nums[index] = nums[right];
-                nums[right] = temp;
-                //index++;  //注意很容易错！index不++的原因是因为与right交换之后，可能把2交换到了index那里，如果这时候index++的话，name这个2就永远都定在那里了，所以需要index不动，目的是为了再访问这个被交换的2一次
-                right--;
-            }
-        }
-    }
-}
 
