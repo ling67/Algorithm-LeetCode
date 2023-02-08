b9f3def973a0bb816cacaa710bbd7844c5f840f3
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-07 18:21:15 -0800

    Update 0621.Task_Scheduler.java

--- Java Solution/0621.Task_Scheduler.java
+++ Java Solution/0621.Task_Scheduler.java
@@ -1,3 +1,4 @@
+//Solution 1: Brute Force
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] count = new int[26];
@@ -15,5 +16,39 @@ class Solution {
         }
         
         return Math.max(tasks.length, (max-1) * (n+1) + maxCount);
+    }
+}
+
+//Solution 2: PriorityQueue
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        Map<Character, Integer> task_counter = new HashMap<>();
+        for(Character task: tasks) {
+            task_counter.put(task, task_counter.getOrDefault(task,0) + 1);    
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        pq.addAll(task_counter.values());
+        
+        int time = 0;
+        while(pq.size() > 0) {
+            List<Integer> add_back = new ArrayList<>();
+            
+            for(int i= 0; i <= n; i++) {
+                if(pq.size() > 0) {
+                    int count = pq.poll(); 
+                    count--;
+                    if(count > 0) { 
+                        add_back.add(count);
+                    }  
+                }
+                
+                time += 1; 
+                if(pq.size() == 0 && add_back.size() == 0) { 
+                    break;
+                }  
+            }   
+            pq.addAll(add_back);  
+        }
+        return time;
     }
 }
