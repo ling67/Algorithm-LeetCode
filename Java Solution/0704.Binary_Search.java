4bd52223f166dc6e494a1752df0dac57ab8c2199
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-13 14:50:27 -0800

    Create 0704.Binary_Search.java

---
+++ Java Solution/0704.Binary_Search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        if (nums[l] == target) {
+            return l;
+        }
+        if (nums[r] == target) {
+            return r;
+        }
+        return -1;
+    }
+}
