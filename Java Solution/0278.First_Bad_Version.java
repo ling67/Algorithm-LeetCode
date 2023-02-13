9eb9dc57d319ef72f0198e7ffc40642d164b963d
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-13 14:55:53 -0800

    Create 0278.First_Bad_Version.java

---
+++ Java Solution/0278.First_Bad_Version.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l + 1 < r) {
+            int mid = l + (r - l) / 2;
+            if (isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+        if (isBadVersion(l)) {
+            return l;
+        } else {
+            return r;
+        }
+    }
+}
