ae0797581cc9175fdf8c23f167b530fdafc005a3
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-14 15:08:58 -0800

    Create 0207.Course_Schedule.java

---
+++ Java Solution/0207.Course_Schedule.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        
+        //map each vertex to its list of adjacent vertices
+        HashMap<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>(); 
+        for(int i = 0; i < numCourses; i++) 
+        map.put(i, new LinkedList<Integer>());
+
+        //record the in-degree of each vertex
+        int[] indegree = new int[numCourses]; 
+
+        for(int[] edge : prerequisites){
+            indegree[edge[0]]++;
+            map.get(edge[1]).add(edge[0]);
+        }
+
+        //determine all vertices with zero in-degree
+        Queue<Integer> zeroVerts = new LinkedList<Integer>(); 
+        for(int i = 0; i < numCourses; i++)
+            if(indegree[i] == 0) 
+               zeroVerts.offer(i);
+
+        //number of courses could be put into the topological order
+        int courseOrder = 0; 
+        while(!zeroVerts.isEmpty()){
+            int course = zeroVerts.poll(); 
+            courseOrder++;
+            //"remove" vertex by updating its neighbors
+	        for(int c : map.get(course)){
+	            indegree[c]--;
+                //add any vertex that now has 0 in-degree to the queue
+	            if(indegree[c] == 0) 
+	               zeroVerts.offer(c);
+	        }
+        }
+        return courseOrder == numCourses; 
+    }
+}
