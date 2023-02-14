383d246278a47a1357cca1d5e452d3874b4bfce5
ling67 <93315926+ling67@users.noreply.github.com>
2023-02-14 12:29:56 -0800

    Create 0046.Permutations.java

---
+++ Java Solution/0046.Permutations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) return res;
+        helper(nums, new ArrayList<Integer>(), res);
+        return res;
+    }
+
+    private void helper(int[] nums, List<Integer> permutation, List<List<Integer>> res) {
+        if (permutation.size() == nums.length) {
+            res.add(new ArrayList<Integer>(permutation));
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (permutation.contains(nums[i])) continue;
+            permutation.add(nums[i]);
+            helper(nums, permutation, res);
+            permutation.remove(permutation.size() - 1);
+        }
+    }
+}
